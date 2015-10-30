a1c4b9d59c4b3930ce9e2b9a7413c5e854e0ed26
Vladimir V. Tuporshin <vofka@vofka.ru>
2015-10-30 14:35:21 +0300

    pl backend sketching

---
+++ js/pocketlists.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.pl-item-details').click(function(){
+    
+        alert('справа будет выезжать сайдбар со свойствами айтема');
+        
+        return false;
+    
+    });
+
+});
