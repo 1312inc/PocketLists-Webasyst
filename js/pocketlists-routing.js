5fa58fd06bd2eb083ed6a546bd5f98a3ab82eace
Kirill Maramygin <kirill.maramygin@gmail.com>
2015-11-06 07:38:04 +0300

    js. routing fish;

---
+++ js/pocketlists-routing.js
@@ -0,0 +1,105 @@
+(function ($) {
+    'use strict';
+
+    $.pocketlists_routing = {
+        options: {},
+        init: function () {
+            var that = this;
+            that.options = options;
+            if (typeof($.History) != "undefined") {
+                $.History.bind(function () {
+                    that.dispatch();
+                });
+            }
+
+            var hash = window.location.hash;
+            if (hash === '#/' || !hash) {
+                this.dispatch();
+            } else {
+                $.wa.setHash(hash);
+            }
+        },
+        // dispatch() ignores the call if prevHash == hash
+        prevHash: null,
+        hash: null,
+
+        // if this is > 0 then this.dispatch() decrements it and ignores a call
+        skipDispatch: 0,
+        /** Cancel the next n automatic dispatches when window.location.hash changes */
+        stopDispatch: function (n) {
+            this.skipDispatch = n;
+        },
+        /** Implements #hash-based navigation. Called every time location.hash changes. */
+        dispatch: function (hash) {
+            if (this.skipDispatch > 0) {
+                this.skipDispatch--;
+                return false;
+            }
+            if (hash === undefined || hash === null) {
+                hash = window.location.hash;
+            }
+            hash = hash.replace(/(^[^#]*#\/*|\/$)/g, ''); /* fix syntax highlight*/
+            if (this.hash !== null) {
+                this.prevHash = this.hash;
+            }
+            this.hash = hash;
+            if (hash) {
+                hash = hash.split('/');
+                if (hash[0]) {
+                    var actionName = "";
+                    var attrMarker = hash.length;
+                    var lastValidActionName = null;
+                    var lastValidAttrMarker = null;
+                    for (var i = 0; i < hash.length; i++) {
+                        var h = hash[i];
+                        if (i < 2) {
+                            if (i === 0) {
+                                actionName = h;
+                            } else if (parseInt(h, 10) != h && h.indexOf('=') == -1) {
+                                actionName += h.substr(0,1).toUpperCase() + h.substr(1);
+                            } else {
+                                break;
+                            }
+                            if (typeof(this[actionName + 'Action']) == 'function') {
+                                lastValidActionName = actionName;
+                                lastValidAttrMarker = i + 1;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                    attrMarker = i;
+
+                    if (typeof(this[actionName + 'Action']) !== 'function' && lastValidActionName) {
+                        actionName = lastValidActionName;
+                        attrMarker = lastValidAttrMarker;
+                    }
+
+                    var attr = hash.slice(attrMarker);
+                    this.preExecute(actionName);
+                    if (typeof(this[actionName + 'Action']) == 'function') {
+                        $.shop.trace('$.orders.dispatch',[actionName + 'Action',attr]);
+                        this[actionName + 'Action'].apply(this, attr);
+                    } else {
+                        $.shop.error('Invalid action name:', actionName+'Action');
+                    }
+                    this.postExecute(actionName);
+                } else {
+                    this.preExecute();
+                    this.defaultAction();
+                    this.postExecute();
+                }
+            } else {
+                this.preExecute();
+                this.defaultAction();
+                this.postExecute();
+            }
+        },
+        preExecute: function () {
+
+        },
+        postExecute: function () {
+
+        }
+    }
+}(jQuery));
