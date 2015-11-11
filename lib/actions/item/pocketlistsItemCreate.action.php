51ce59d69aa08c540ccdefbeb52eea04483cfaa9
Kirill Maramygin <kirill.maramygin@gmail.com>
2015-11-12 02:21:19 +0300

    add each line as new item;

---
+++ lib/actions/item/pocketlistsItemCreate.action.php
@@ -0,0 +1,26 @@
+<?php
+
+class pocketlistsItemCreateAction extends waViewAction
+{
+    public function execute()
+    {
+        $data = waRequest::post('data', false, waRequest::TYPE_ARRAY);
+        $list_id = waRequest::post('list_id', false, waRequest::TYPE_INT);
+
+        $im = new pocketlistsItemModel();
+        $items = array();
+        if ($list_id && $data) {
+            if (!is_array($data)) {
+                $data = array($data);
+            }
+            foreach ($data as $i => $d) {
+                $data[$i]['create_datetime'] = date("Y-m-d H:i:s");
+                $data[$i]['list_id'] = $list_id;
+
+                $inserted = $im->insert($data[$i], 1);
+                $items[] = array('id' => $inserted) + $data[$i];
+            }
+        }
+        $this->view->assign('item', $items);
+    }
+}
