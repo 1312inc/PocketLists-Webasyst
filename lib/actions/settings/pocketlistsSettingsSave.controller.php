89ac2be39dcd2b9e7f5a23da2aa98c815818019f
Kirill Maramygin <kirill.maramygin@gmail.com>
2015-12-11 18:26:47 +0300

    save user settings;

--- lib/actions/settings/pocketlistsSettingsSave.controller.php
+++ lib/actions/settings/pocketlistsSettingsSave.controller.php
@@ -7,7 +7,15 @@ class pocketlistsSettingsSaveController extends  waJsonController
         $cs = new waContactSettingsModel();
         $app_name = wa()->getApp();
 
-        $data = waRequest::post();
+        $data = array_merge(array(
+            'daily_recap_on' => 0,
+            'email_assign_me' => 0,
+            'email_complete_item_on' => 0,
+            'email_add_item_on' => 0,
+            'email_comment_item_on' => 0,
+            'email_create_list_on' => 0
+        ), waRequest::post());
+        // update new
         foreach($data as $name => $value) {
             $cs->set(wa()->getUser()->getId(), $app_name, $name, $value);
         }
