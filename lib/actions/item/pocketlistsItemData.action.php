22760f120a807025411439b31ba034ef127c0985
Kirill Maramygin <kirill.maramygin@gmail.com>
2015-12-16 02:06:55 +0300

    add due time select to item details;

--- lib/actions/item/pocketlistsItemData.action.php
+++ lib/actions/item/pocketlistsItemData.action.php
@@ -10,17 +10,14 @@ class pocketlistsItemDataAction extends waViewAction
                 $im = new pocketlistsItemModel();
 
                 if ($item['due_date'] && !empty($item['due_datetime_hours']) && !empty($item['due_datetime_minutes']))  {
-                    $item['due_datetime'] = @waDateTime::format('Y-m-d H:i:00', strtotime($item['due_date']." ".$item['due_datetime_hours'].":".$item['due_datetime_minutes'].":00"));
+                    $item['due_datetime'] = date('Y-m-d H:i:00', strtotime($item['due_date']." ".$item['due_datetime_hours'].":".$item['due_datetime_minutes'].":00"));
                     unset($item['due_datetime_hours']);
                     unset($item['due_datetime_minutes']);
                 } else {
                     $item['due_datetime'] = null;
                 }
 
-                $item['due_date'] = $item['due_date'] ? @waDateTime::format(
-                    'Y-m-d',
-                    strtotime($item['due_date'])
-                ) : null;
+                $item['due_date'] = $item['due_date'] ? date('Y-m-d', strtotime($item['due_date'])) : null;
 
                 $item['update_datetime'] = date("Y-m-d H:i:s");
 
