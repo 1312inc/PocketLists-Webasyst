a4c485c965c7e6b0f2ae4b41b907cb92cd9bda68
Kirill Maramygin <kirill.maramygin@gmail.com>
2016-02-14 20:42:17 +0300

    some 403 error fix i hope

--- lib/actions/pocket/pocketlistsPocket.action.php
+++ lib/actions/pocket/pocketlistsPocket.action.php
@@ -8,7 +8,7 @@ class pocketlistsPocketAction extends waViewAction
         $list_id = waRequest::get('list_id', false, waRequest::TYPE_INT);
 
         $available_pockets = pocketlistsHelper::getAccessPocketForContact();
-        if (!$available_pockets) {
+        if (!in_array($id, $available_pockets)) {
             throw new waException('Access denied.', 403);
         }
 
