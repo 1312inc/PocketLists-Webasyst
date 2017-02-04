0bd9478a3495cc75e61e530f4556d36afd8f9cb2
Kirill Maramygin <kirill.maramygin@gmail.com>
2017-02-04 08:52:16 +0300

    3.97: add item notice

--- lib/actions/item/pocketlistsItemCreate.action.php
+++ lib/actions/item/pocketlistsItemCreate.action.php
@@ -103,7 +103,7 @@ class pocketlistsItemCreateAction extends waViewAction
                 }
 
                 // log this action
-                $this->logAction(pocketlistsLogAction::NEW_ITEMS, array('list_id' => $list['id']));
+                $this->logAction(pocketlistsLogAction::NEW_ITEMS, array('list_id' => $list ? $list['id'] : null));
             }
         }
 
