dcad0bc42528b5830a225ce6250986be496df349
Kirill Maramygin <kirill.maramygin@gmail.com>
2017-03-05 16:28:30 +0300

    3.170: fresh install > new list name bug

--- lib/config/install.php
+++ lib/config/install.php
@@ -1,9 +1,10 @@
 <?php
 $lm = new pocketlistsListModel();
 $lm->add(array(
-    'name' => wa()->accountName(),
+    'name'            => wa()->accountName(),
+    'type'            => 'checklist',
     'create_datetime' => date('Y-m-d H:i:s'),
-    'contact_id' => wa()->getUser()->getId()
+    'contact_id'      => wa()->getUser()->getId(),
 ));
 $pr = new pocketlistsRightConfig();
 $pr->setRights(wa()->getUser()->getId(), 'backend', 2);
