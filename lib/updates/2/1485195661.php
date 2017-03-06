3e2cbdea8dfa15270c0de9b16fa12efda702be0c
vofka <vofka@webasyst.com>
2017-03-06 09:57:56 +0300

    removed lib/update/ duplicating string
    reported by moderation team

--- lib/updates/2/1485195661.php
+++ lib/updates/2/1485195661.php
@@ -2,6 +2,5 @@
 // pocketsbye update
 waFiles::delete(wa()->getAppPath('lib/model/pocketlistsPocket.model.php', 'pocketlists'));
 waFiles::delete(wa()->getAppPath('lib/model/pocketlistsPocketRights.model.php', 'pocketlists'));
-waFiles::delete(wa()->getAppPath('lib/model/pocketlistsPocketRights.model.php', 'pocketlists'));
 waFiles::delete(wa()->getAppPath('templates/actions/pocket', 'pocketlists'), true);
 waFiles::delete(wa()->getAppPath('lib/actions/pocket', 'pocketlists'), true);
