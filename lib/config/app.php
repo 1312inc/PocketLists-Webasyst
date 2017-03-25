d7388eac9bb4df4073134b17961c4bd478ee8045
Kirill Maramygin <kirill.maramygin@gmail.com>
2017-03-25 21:33:41 +0300

    3.191: disable frontend

--- lib/config/app.php
+++ lib/config/app.php
@@ -1,15 +1,18 @@
 <?php
-return array (
-  'name' =>  _w('To-dos'),
-  'icon' =>
-  array (
-    48 => 'img/pocketlists48.png',
-    96 => 'img/pocketlists96.png',
-  ),
-  'version' => '2.0.0',
-  'vendor' => '1021997',
-  'frontend' => true,
-  'plugins' => false,
-  'rights' => true,
-  'sash_color' => '#48b7f0'
+return array(
+    'name'           => _w('To-dos'),
+    'icon'           =>
+        array(
+            48 => 'img/pocketlists48.png',
+            96 => 'img/pocketlists96.png',
+        ),
+    'version'        => '2.0.0',
+    'vendor'         => '1021997',
+    'frontend'       => true,
+    'routing_params' => array(
+        'private' => true
+    ),
+    'plugins'        => false,
+    'rights'         => true,
+    'sash_color'     => '#48b7f0',
 );
