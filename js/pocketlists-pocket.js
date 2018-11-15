f19d00e1e0e6adefc23428c2677950fca3494022
Kirill Maramygin <kirill.maramygin@gmail.com>
2018-11-15 15:41:43 +0300

    wip: new pocket

---
+++ js/pocketlists-pocket.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+/**
+ * object to work with list
+ * - add
+ * - print
+ * - delete
+ * - archive
+ * - sort all
+ */
+$.pocketlists.Pocket = function ($pocket_wrapper, options) {
+
+    function init() {
+    }
+
+    function addHandlers() {
+    }
+};
