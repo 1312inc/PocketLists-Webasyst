6d858326812b05cec33bbd5e4c3053c67d2961bd
vofka <vofka@webasyst.com>
2018-12-29 09:15:20 +0300

    3.1.0 localization & minor tweaks

--- lib/actions/list/pocketlistsListMoveTo.controller.php
+++ lib/actions/list/pocketlistsListMoveTo.controller.php
@@ -16,7 +16,7 @@ class pocketlistsListMoveToController extends waJsonController
         $pocket_id = waRequest::post('pocket_id', 0, waRequest::TYPE_INT);
 
         if (!$id || !$pocket_id) {
-            $this->setError(_w('No pocket id or list id params'));
+            $this->setError(_w('No pocket/id and list/id params specified'));
 
             return;
         }
@@ -27,7 +27,7 @@ class pocketlistsListMoveToController extends waJsonController
         $list = pocketlistsListModel::model()->findByPk($id);
 
         if (!$pocket || !$list) {
-            $this->setError(_w('No pocket or list found'));
+            $this->setError(_w('Pocket and list not found'));
 
             return;
         }
