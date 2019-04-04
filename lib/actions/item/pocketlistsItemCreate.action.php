97ce6bc7f78a8ae089647ef388c0de5087af7cc6
Kirill Maramygin <marmygin@smartcredit.ru>
2019-04-04 10:17:57 +0300

    php wipL create item and notify about this

--- lib/actions/item/pocketlistsItemCreate.action.php
+++ lib/actions/item/pocketlistsItemCreate.action.php
@@ -16,15 +16,14 @@ class pocketlistsItemCreateAction extends pocketlistsViewAction
         $list_id = waRequest::post('list_id', false, waRequest::TYPE_INT);
         $assigned_contact_id = waRequest::post('assigned_contact_id', false, waRequest::TYPE_INT);
 
-        $im = new pocketlistsItemModel();
-        $inserted = $inserted_items = $items = [];
+        $inserted = $items = [];
         $assign_contact = null;
         $user_id = wa()->getUser()->getId();
         $plContact = new pocketlistsContact(new waContact($user_id));
 
         $canAssign = pocketlistsRBAC::canAssign();
         if ($canAssign && $assigned_contact_id) {
-            $assign_contact = new waContact($assigned_contact_id);
+            $assign_contact = new pocketlistsContact(new waContact($assigned_contact_id));
         }
 
         // if no list id passed - get default list from settings
@@ -42,16 +41,11 @@ class pocketlistsItemCreateAction extends pocketlistsViewAction
             }
 
             /** @var pocketlistsListFactory $listFactory */
-            $lm = new pocketlistsListModel();
-            $list = $lm->getById($list_id);
-
-            /** @var pocketlistsListFactory $listFactory */
             $listFactory = pl2()->getEntityFactory(pocketlistsList::class);
             /** @var pocketlistsItemFactory $itemFactory */
             $itemFactory = pl2()->getEntityFactory(pocketlistsItem::class);
 
             $list = $list_id ? $listFactory->findById($list_id) : $listFactory->createNewNullList();
-
 
             foreach ($data as $i => $d) {
                 /** @var pocketlistsItem $item */
@@ -61,16 +55,12 @@ class pocketlistsItemCreateAction extends pocketlistsViewAction
                     ->setList($list)
                     ->setContactId($user_id);
 
-                $data[$i]['create_datetime'] = date("Y-m-d H:i:s");
-                $data[$i]['list_id'] = $list ? $list['id'] : null;
-                $data[$i]['contact_id'] = $user_id;
-
                 if ($canAssign && ($assigned_contact_id || !empty($d['assigned_contact_id']))) {
                     if (!empty($d['assigned_contact_id'])) {
                         $assign_contact = new pocketlistsContact(new waContact($d['assigned_contact_id']));
                     }
 
-                    if ($assign_contact->exists()) {
+                    if ($assign_contact->isExists()) {
                         $item->setAssignedContact($assign_contact);
                     }
                 }
@@ -161,25 +151,25 @@ class pocketlistsItemCreateAction extends pocketlistsViewAction
                 }
 
                 $inserted[] = $item->getId();
-                $inserted_items[] = $item;
+                $items[] = $item;
             }
 
             if ($inserted) {
                 switch ($filter) {
                     case 'favorites':
                         /** @var pocketlistsItem $item */
-                        foreach ($inserted_items as $item) {
+                        foreach ($items as $item) {
                             $item->makeFavorite($plContact);
                         }
                         break;
                 }
 
-                pocketlistsNotifications::notifyAboutNewItems($items, $list);
+                (new pocketlistsNotificationAboutNewItems())->notify($items, $list);
 
                 // log this action
                 foreach ($items as $item) {
                     if ($assign_contact) {
-                        pocketlistsNotifications::notifyAboutNewAssign($item);
+                        (new pocketlistsNotificationAboutNewAssign())->notify($item, $list);
 
                         $this->logAction(
                             pocketlistsLogAction::ITEM_ASSIGN_TEAM,
---
+++ lib/class/Notification/pocketlistsNotificationAboutNewItems.class.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * Class pocketlistsNotificationsAboutNewItems
+ */
+class pocketlistsNotificationAboutNewItems extends pocketlistsNotification
+{
+    /**
+     * Notify all related users about new items (according to their settings)
+     *
+     * @param pocketlistsItem[]    $items
+     * @param pocketlistsList|null $list
+     *
+     * @throws waDbException
+     * @throws waException
+     */
+    public function notify($items, pocketlistsList $list = null)
+    {
+        if (!$items) {
+            return;
+        }
+
+        if (!is_array($items)) {
+            $items = [$items];
+        }
+
+        $csm = new waContactSettingsModel();
+        $q = "SELECT
+                cs1.contact_id contact_id,
+                cs2.value setting
+              FROM wa_contact_settings cs1
+              LEFT JOIN wa_contact_settings cs2 ON
+                cs1.contact_id = cs2.contact_id
+                AND cs2.app_id = s:app_id
+                AND cs2.name = 'email_add_item'
+                AND cs2.value IN (i:setting)
+              WHERE
+                cs1.app_id = s:app_id
+                AND cs1.name = 'email_add_item_on'
+                AND cs1.value = 1";
+        $users = $csm->query(
+            $q,
+            [
+                'app_id'  => wa()->getApp(),
+                'setting' => [
+                    pocketlistsUserSettings::EMAIL_WHEN_SOMEONE_ADDS_ITEM_TO_FAVORITE_LIST,
+                    pocketlistsUserSettings::EMAIL_WHEN_SOMEONE_ADDS_ITEM_TO_ANY_LIST,
+                ],
+            ]
+        )->fetchAll('contact_id');
+
+        /** @var pocketlistsUserFavoritesModel $ufm */
+        $ufm = pl2()->getModel('pocketlistsUserFavorites');
+
+        foreach ($users as $user_id => $user) { // foreach user
+            $contact = new pocketlistsContact(new waContact($user_id));
+            if (!$this->canSend($contact)) {
+                continue;
+            }
+
+            $filtered_items = [];
+            $emailParams = [];
+
+            switch ($user['setting']) {
+                case pocketlistsUserSettings::EMAIL_WHEN_SOMEONE_ADDS_ITEM_TO_FAVORITE_LIST:
+                    $user_lists = $ufm->query(
+                        "SELECT i.key_list_id FROM {$ufm->getTableName()} uf JOIN pocketlists_item i ON uf.item_id = i.id AND i.key_list_id > 0 WHERE uf.contact_id = {$user_id}"
+                    )->fetchAll('key_list_id');
+                    $user_lists = array_keys($user_lists);
+
+                    foreach ($items as $item) {
+                        if (!$this->checkItem($item, $user_id)) {
+                            continue;
+                        }
+
+                        $filtered_items[$item->getId()] = $item;
+                    }
+
+                    break;
+
+                case pocketlistsUserSettings::EMAIL_WHEN_SOMEONE_ADDS_ITEM_TO_ANY_LIST:
+                    foreach ($items as $item) {
+                        if (!$this->checkItem($item, $user_id)) {
+                            continue;
+                        }
+
+                        $filtered_items[$item->getId()] = $item;
+                    }
+
+                    break;
+            }
+
+            if ($filtered_items && $list) {
+                $emailParams = [
+                    'subject'    => 'string:{str_replace(array("\r", "\n"), " ", $item->getName())|truncate:64}',
+                    'body'       => wa()->getAppPath('templates/mails/newitem.html'),
+                    'contact_id' => $user_id,
+                    'variables'  => [
+                        'list_name' => $list->getId() ? $list->getName() : false,
+                        'list_url'  => $list ? sprintf(
+                            '#/pocket/%s/list/%s/',
+                            $list->getPocketId(),
+                            $list->getId()
+                        ) : '',
+                        'items'     => $filtered_items,
+                        'item'      => new pocketlistsItemOutputDecorator(reset($filtered_items)),
+                    ],
+                ];
+
+                $this->sendMail($emailParams, $this->getBackendUrl($user_id));
+            }
+        }
+    }
+
+    /**
+     * @param pocketlistsItem $item
+     * @param                 $user_id
+     *
+     * @return bool
+     * @throws waException
+     */
+    private function checkItem(pocketlistsItem $item, $user_id)
+    {
+        // created not by this user
+        if ($item->getContactId() == $user_id) {
+            return false;
+        }
+
+        $list = $this->getList($item);
+
+        if($list->getId() && !pocketlistsRBAC::canAccessToList($list, $user_id)) {
+            return false;
+        }
+
+        // from NULL-list but not assigned to this user nor created by user
+        if (!$list->getId() && ($item->getAssignedContactId() != $user_id || $item->getContactId() != $user_id)) {
+            return false;
+        }
+
+        return true;
+    }
+}
