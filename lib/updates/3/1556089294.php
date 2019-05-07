d96da5ebcf5b4a72a72884c729be058a9ea469e2
Kirill Maramygin <marmygin@smartcredit.ru>
2019-05-07 14:39:40 +0300

    notification send errors

--- lib/updates/3/1556089294.php
+++ lib/updates/3/1556089294.php
@@ -3,17 +3,16 @@
 $m = new pocketlistsModel();
 
 try {
-    $m->exec(
-        'create table pocketlists_notification
+    $m->exec('create table pocketlists_notification
         (
             id         int auto_increment  primary key,
             type       smallint(6)          not null,
             created_at datetime             not null,
             sent_at    datetime             null,
             status     varchar(255)         null,
-            error      text charset utf8mb4 null,
-            data       text charset utf8mb4 null
+            error      text                 null,
+            data       text                 null
         )');
 } catch (Exception $ex) {
-    waLog::log('Error on creating pocketlists_notification.', 'pocketlists/update.log');
+    waLog::log('Error on creating pocketlists_notification table.', 'pocketlists/update.log');
 }
--- lib/updates/3/1556089295.php
+++ lib/updates/3/1556089295.php
@@ -5,5 +5,5 @@ $m = new pocketlistsModel();
 try {
     $m->exec('create index pocketlists_notification_status_index on pocketlists_notification (status)');
 } catch (Exception $ex) {
-    waLog::log('Error on creating pocketlists_notification.', 'pocketlists/update.log');
+    waLog::log('Error on creating pocketlists_notification index.', 'pocketlists/update.log');
 }
