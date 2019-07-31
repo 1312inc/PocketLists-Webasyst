7d10620b6dda54917e177c47162dcc10851055f9
Kirill Maramygin <kirill.maramygin@gmail.com>
2019-07-31 09:04:45 +0300

    3.504: status labels: pre-defined status labels @ pl2pro plugin

--- plugins/pro/lib/config/install.php
+++ plugins/pro/lib/config/install.php
@@ -7,12 +7,14 @@ $m = new pocketlistsModel();
 $m->exec("alter table pocketlists_item add pro_label int default null");
 
 $labels = [
-    ['Backlog', 'aaaaaa'],
-    ['To do', '46c7fc'],
-    ['In progress', '328532'],
-    ['Testing', 'ffc600'],
+    [_wp('Backlog'), 'aaaaaa'],
+    [_wp('To do'), '46c7fc'],
+    [_wp('In progress'), '328532'],
+    [_wp('Testing'), 'ffc600'],
 ];
 
 foreach ($labels as $label) {
-    $m->exec(sprintf("insert into pocketlists_pro_label (name, color, sort) values ('%s', '%s', null)", $label[0], $label[1]));
+    $m->exec(
+        sprintf("insert into pocketlists_pro_label (name, color, sort) values ('%s', '%s', null)", $label[0], $label[1])
+    );
 }
