649d2badf99fb14e3c271831a7c42d9c9ebbf2a8
Kirill Maramygin <kirill.maramygin@gmail.com>
2019-08-08 23:44:21 +0300

    3.518: status labels @ item add/edit

---
+++ plugins/pro/lib/classes/EventListener/pocketlistsProPluginWaEventListener.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Class pocketlistsProPluginWaEventListener
+ */
+class pocketlistsProPluginWaEventListener
+{
+    /**
+     * @param array $eventData
+     *
+     * @return mixed
+     */
+    public function onEntityInsertBefore(array $eventData)
+    {
+        $object = $eventData['entity'];
+        $eventName = '';
+
+        switch (get_class($object)) {
+            case pocketlistsItem::class:
+                $eventName = pocketlistsEventStorage::ITEM_INSERT;
+        }
+
+        return pl2()->event($eventName, $object, $eventData['data']);
+    }
+
+    /**
+     * @param array $eventData
+     *
+     * @return mixed
+     */
+    public function onEntityUpdateBefore(array $eventData)
+    {
+        $object = $eventData['entity'];
+        $eventName = '';
+
+        switch (get_class($object)) {
+            case pocketlistsItem::class:
+                $eventName = pocketlistsEventStorage::ITEM_UPDATE;
+        }
+
+        return pl2()->event($eventName, $object, $eventData['data']);
+    }
+}
--- plugins/pro/lib/pocketlistsProPlugin.class.php
+++ plugins/pro/lib/pocketlistsProPlugin.class.php
@@ -27,7 +27,7 @@ class pocketlistsProPlugin extends waPlugin
      */
     public function backendHeadHandler()
     {
-        return $this->getView()->fetch($this->getViewTemplate('backend_head.html'));
+        return $this->getView()->fetch($this->getViewTemplate('backend_head'));
     }
 
     /**
@@ -35,7 +35,7 @@ class pocketlistsProPlugin extends waPlugin
      */
     public function backendSettingsHandler()
     {
-        $sidebarLi = $this->getView()->fetch($this->getViewTemplate('backend_settings.sidebar_li.html'));
+        $sidebarLi = $this->getView()->fetch($this->getViewTemplate('backend_settings.sidebar_li'));
 
         return [
             'sidebar_li' => $sidebarLi,
@@ -51,7 +51,7 @@ class pocketlistsProPlugin extends waPlugin
 
         $hooks = ['streams_li', 'views_li', 'section_block', 'system_li'];
         foreach ($hooks as $hook) {
-            $return[$hook] = $this->getView()->fetch($this->getViewTemplate('backend_sidebar.'.$hook.'.html'));
+            $return[$hook] = $this->getView()->fetch($this->getViewTemplate('backend_sidebar.'.$hook));
         }
 
         return $return;
@@ -66,23 +66,43 @@ class pocketlistsProPlugin extends waPlugin
 
         $hooks = ['sidebar_section'];
         foreach ($hooks as $hook) {
-            $return[$hook] = $this->getView()->fetch($this->getViewTemplate('backend_pocket.'.$hook.'.html'));
+            $return[$hook] = $this->getView()->fetch($this->getViewTemplate('backend_pocket.'.$hook));
         }
 
         return $return;
     }
 
     /**
+     * @param null|pocketlistsItem $item
+     *
      * @return array
+     * @throws waException
      */
-    public function backendItemAddHandler()
+    public function backendItemAddHandler($item)
     {
         $return = [];
 
-        $hooks = ['compact'];
-        foreach ($hooks as $hook) {
-            $return[$hook] = $this->getView()->fetch($this->getViewTemplate('backend_item_add.'.$hook.'.html'));
+        /** @var pocketlistsProPluginLabelFactory $factoryLabel */
+        $factoryLabel = pl2()->getEntityFactory(pocketlistsProPluginLabel::class);
+        /** @var pocketlistsProPluginShortcutFactory $factoryShortcut */
+        $factoryShortcut = pl2()->getEntityFactory(pocketlistsProPluginShortcut::class);
+
+        $label = null;
+        if ($item instanceof pocketlistsItem) {
+            $label = $factoryLabel->findForItem($item);
         }
+        $label = $label ?: new pocketlistsProPluginLabel();
+
+        $this->getView()->assign(
+            [
+                'itemLabel' => $label,
+                'labels'    => $factoryLabel->findAll(),
+                'shortcutsGrouped' => $factoryShortcut->findAllGrouped(),
+            ]
+        );
+
+        $return['compact'] = $this->getView()->fetch($this->getViewTemplate('backend_item_add'));
+        $return['detail'] = $return['compact'];
 
         return $return;
     }
@@ -94,7 +114,7 @@ class pocketlistsProPlugin extends waPlugin
      */
     protected function getViewTemplate($name)
     {
-        return sprintf('%s/templates/hooks/%s', $this->path, $name);
+        return sprintf('%s/templates/hooks/%s.html', $this->path, $name);
     }
 
     public function saveEntity(pocketlistsEventInterface $event)
