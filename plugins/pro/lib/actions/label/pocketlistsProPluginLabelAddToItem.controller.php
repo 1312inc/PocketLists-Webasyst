69d845834f31e286559a58d567d30acac8d8ea9b
Kirill Maramygin <kirill.maramygin@gmail.com>
2019-08-22 09:58:56 +0300

    3.512: agile board

---
+++ plugins/pro/lib/actions/label/pocketlistsProPluginLabelAddToItem.controller.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Class pocketlistsProPluginLabelAddToItemController
+ */
+class pocketlistsProPluginLabelAddToItemController extends pocketlistsJsonController
+{
+    /**
+     * @throws waException
+     */
+    public function execute()
+    {
+        $resolver = new pocketlistsEntityResolver();
+
+        $labelId = waRequest::post('id', 0, waRequest::TYPE_INT);
+
+        /** @var pocketlistsProPluginLabel $label */
+        if ($labelId) {
+            $label = $resolver->getEntityById(pocketlistsProPluginLabel::class, $labelId);
+        } else {
+            $label= pl2()->getEntityFactory(pocketlistsProPluginLabel::class)->createNewDone();
+        }
+
+        /** @var pocketlistsItem $item */
+        $item = $resolver->getEntityById(
+            pocketlistsItem::class,
+            waRequest::post('item_id', 0, waRequest::TYPE_INT)
+        );
+
+        $label->assignToItem($item);
+        pl2()->getEntityFactory(pocketlistsItem::class)->update($item);
+    }
+}
