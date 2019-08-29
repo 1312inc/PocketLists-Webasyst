70fc30e9545aca9d781c72116f51697130dab9f3
Kirill Maramygin <kirill.maramygin@gmail.com>
2019-08-29 11:45:16 +0300

    3.553: agile board: 'All pockets' & selected pocket

--- plugins/pro/lib/actions/board/pocketlistsProPluginBoard.action.php
+++ plugins/pro/lib/actions/board/pocketlistsProPluginBoard.action.php
@@ -13,11 +13,23 @@ class pocketlistsProPluginBoardAction extends pocketlistsProPluginAbstractViewAc
      */
     public function runAction($params = null)
     {
+        $pocketId = waRequest::request('pocket_id', 0, waRequest::TYPE_INT);
+
         /** @var pocketlistsProPluginLabelFactory $labelFactory */
         $labelFactory = pl2()->getEntityFactory(pocketlistsProPluginLabel::class);
+        $allItems = [];
+        $pocket = null;
         $allLabels = $labelFactory->findAll();
 
-        $allItems = [];
+        if ($pocketId) {
+            /** @var pocketlistsPocket $pocket */
+            $pocket = (new pocketlistsEntityResolver())->getEntityById(pocketlistsPocket::class, $pocketId);
+
+            if (!pocketlistsRBAC::contactHasAccessToPocket($pocket)) {
+                throw new pocketlistsForbiddenException();
+            }
+        }
+
         /** @var pocketlistsProPluginLabel $label */
         foreach ($allLabels as $label) {
             $labelItems = new pocketlistsProPluginLabelItemsDto();
@@ -25,7 +37,7 @@ class pocketlistsProPluginBoardAction extends pocketlistsProPluginAbstractViewAc
             $labelItems->items = $labelFactory
                 ->setOffset(0)
                 ->setLimit(50)
-                ->findItemsByLabel($label);
+                ->findItemsByLabelAndPocket($label, $pocket);
             $labelItems->count = count($labelItems->items);
 
             $allItems[$labelItems->label->getId()] = $labelItems;
@@ -36,14 +48,19 @@ class pocketlistsProPluginBoardAction extends pocketlistsProPluginAbstractViewAc
         $labelItems->items = $labelFactory
             ->setOffset(0)
             ->setLimit(50)
-            ->findDoneItemsByAllLabels();
+            ->findDoneItemsByAllLabelsAndPocket();
         $labelItems->count = count($labelItems->items);
         $labelItems->isDone = true;
 
         $allItems[$labelItems->label->getId()] = $labelItems;
 
+        /** @var pocketlistsPocketFactory $pocketFactory */
+        $pocketFactory = pl2()->getEntityFactory(pocketlistsPocket::class);
+
         $this->view->assign(
             [
+                'pockets'  => $pocketFactory->findAllForUser(),
+                'pocket'   => $pocket ?: $pocketFactory->createNew(),
                 'allItems' => $allItems,
             ]
         );
