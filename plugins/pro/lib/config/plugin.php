c98a2a06ca359b0086c27e6a4f00bc06106463f3
vofka <vofka@webasyst.com>
2019-09-13 14:48:50 +0300

    pl2pro config file [] => array() fix

--- plugins/pro/lib/config/plugin.php
+++ plugins/pro/lib/config/plugin.php
@@ -1,66 +1,66 @@
 <?php
-return [
+return array(
     'name'     => 'Pocket Lists PRO',
     'img'      => 'img/pl2pro.png',
     'version'  => '1.0.0',
-    'vendor'   => '--',
+    'vendor'   => '1021997',
     'handlers' =>
-        [
-            '*' => [
-                [
+        array(
+            '*' => array(
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_head',
                     'class'        => 'pocketlistsProPluginHookHandlerHead',
                     'method'       => 'handle',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'entity_insert.before',
                     'class'        => 'pocketlistsProPluginWaEventListener',
                     'method'       => 'onEntityInsertBefore',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'entity_update.before',
                     'class'        => 'pocketlistsProPluginWaEventListener',
                     'method'       => 'onEntityUpdateBefore',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_settings',
                     'class'        => 'pocketlistsProPluginHookHandlerSettings',
                     'method'       => 'handle',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_sidebar',
                     'class'        => 'pocketlistsProPluginHookHandlerSidebar',
                     'method'       => 'handle',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_pocket',
                     'class'        => 'pocketlistsProPluginHookHandlerPocket',
                     'method'       => 'handle',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_item_add',
                     'class'        => 'pocketlistsProPluginHookHandlerItemAdd',
                     'method'       => 'handle',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_list_accesses',
                     'class'        => 'pocketlistsProPluginHookHandlerListAccesses',
                     'method'       => 'handle',
-                ],
-                [
+                ),
+                array(
                     'event_app_id' => 'pocketlists',
                     'event'        => 'backend_teammate_sidebar',
                     'class'        => 'pocketlistsProPluginHookHandlerTeammateSidebar',
                     'method'       => 'handle',
-                ],
-            ],
-        ],
-];
+                ),
+            ),
+        ),
+);
