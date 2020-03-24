2c7881b59dfe1a9c909f4b61faa82a0857a8c6c7
Kirill Maramygin <kirill.maramygin@gmail.com>
2020-03-24 19:13:56 +0300

    3.650:enable file attachments for new to-dos

--- lib/actions/item/pocketlistsItemAddAttachment.controller.php
+++ lib/actions/item/pocketlistsItemAddAttachment.controller.php
@@ -74,89 +74,100 @@ class pocketlistsItemAddAttachmentController extends pocketlistsJsonController
 
         $item_id = waRequest::post('item_id', 0, waRequest::TYPE_INT);
         $filevalid = $this->validFile($uploadedFile->getFile());
-        if ($filevalid === true) {
-            /** @var pocketlistsItemFactory $itemFactory */
-            $itemFactory = pl2()->getEntityFactory(pocketlistsItem::class);
-            /** @var pocketlistsItem $item */
-            $item = $itemFactory->findById($item_id);
-            if ($item instanceof pocketlistsItem) {
-                $uploadedFile->setItemId($item->getId());
-            }
-
-            if ($uploadedFile->getFile()->uploaded()) {
-                $name = $uploadedFile->getName();
-                if (!preg_match('//u', $name)) {
-                    $tmp_name = @iconv('windows-1251', 'utf-8//ignore', $name);
-                    if ($tmp_name) {
-                        $name = $tmp_name;
-                    }
-                }
-
-                if (file_exists($uploadedFile->getFullPath())) {
-                    $i = strrpos($name, '.');
-                    $ext = substr($name, $i + 1);
-                    $name = substr($name, 0, $i);
-                    $i = 1;
-                    while (file_exists(sprintf('%s%s-%s.%s', $uploadedFile->getPath(), $name, $i, $ext))) {
-                        $i++;
-                    }
-
-                    $uploadedFile->setName(sprintf('%s-%s.%s', $name, $i, $ext));
-                }
-
-                $type = null;
-                if (exif_imagetype($uploadedFile->getFile()->tmp_name)) {
-                    $type = 'image';
-                }
-
-                if ($uploadedFile->getFile()->moveTo($uploadedFile->getPath(), $uploadedFile->getName())) {
-                    $return = [];
-                    if (!$uploadedFile->isTemp()) {
-                        /** @var pocketlistsFactory $attachmentFactory */
-                        $attachmentFactory = pl2()->getEntityFactory(pocketlistsAttachment::class);
-                        /** @var pocketlistsAttachment $attachment */
-                        $attachment = $attachmentFactory->createNew();
-                        $attachment
-                            ->setFilename($uploadedFile->getName())
-                            ->setFiletype($type)
-                            ->setItemId($item->getId());
-
-                        $attachmentFactory->insert($attachment);
-
-                        $this->logService->add(
-                            $this->logService->getFactory()->createNewAttachmentLog(
-                                (new pocketlistsLogContext())
-                                    ->setItem($item)
-                                    ->setAttachment($attachment)
-                            )
-                        );
-
-                        $this->errors = [];
-
-                        $return = ['id' => $attachment->getId()];
-                    }
-
-                    return array_merge(
-                        [
-                            'url' => $uploadedFile->getUrl(),
-                            'name' => $uploadedFile->getName(),
-                            'type' => $uploadedFile->getType(),
-                            'size' => $uploadedFile->getSize(),
-                            'id' => -1,
-                        ],
-                        $return
-                    );
-                }
-
-                $this->errors[] = sprintf(_w('Failed to upload file %s.'), $file->name);
-            } else {
-                $this->errors[] = sprintf(_w('Failed to upload file %s.'), $file->name).' ('.$file->error.')';
-            }
-        } else {
+        if ($filevalid !== true) {
             $this->errors[] = $filevalid;
+
+            return false;
         }
 
-        return false;
+        /** @var pocketlistsItemFactory $itemFactory */
+        $itemFactory = pl2()->getEntityFactory(pocketlistsItem::class);
+        /** @var pocketlistsItem $item */
+        $item = $itemFactory->findById($item_id);
+        if ($item instanceof pocketlistsItem) {
+            $uploadedFile->setItemId($item->getId());
+        }
+
+        if (!$uploadedFile->getFile()->uploaded()) {
+            $this->errors[] = sprintf(_w('Failed to upload file %s.'), $file->name).' ('.$file->error.')';
+
+            return false;
+        }
+
+        $name = $uploadedFile->getName();
+        if (!preg_match('//u', $name)) {
+            $tmp_name = @iconv('windows-1251', 'utf-8//ignore', $name);
+            if ($tmp_name) {
+                $name = $tmp_name;
+            }
+        }
+
+        if (file_exists($uploadedFile->getFullPath())) {
+            $i = strrpos($name, '.');
+            $ext = substr($name, $i + 1);
+            $name = substr($name, 0, $i);
+            $i = 1;
+            while (file_exists(sprintf('%s%s-%s.%s', $uploadedFile->getPath(), $name, $i, $ext))) {
+                $i++;
+            }
+
+            $uploadedFile->setName(sprintf('%s-%s.%s', $name, $i, $ext));
+        }
+
+        $type = null;
+        if (exif_imagetype($uploadedFile->getFile()->tmp_name)) {
+            $type = 'image';
+        }
+
+        if (!waFiles::create($uploadedFile->getPath(), true)) {
+            $this->errors[] = sprintf(_w('Failed to upload file %s. Check permissions.'), $file->name);
+            pocketlistsLogger::error(sprintf('Failed to upload file %s. Check permissions.', $uploadedFile->getPath()));
+
+            return false;
+        }
+
+        if (!$uploadedFile->getFile()->moveTo($uploadedFile->getPath(), $uploadedFile->getName())) {
+            $this->errors[] = sprintf(_w('Failed to upload file %s.'), $file->name);
+
+            return false;
+        }
+
+        $return = [];
+        if (!$uploadedFile->isTemp()) {
+            /** @var pocketlistsFactory $attachmentFactory */
+            $attachmentFactory = pl2()->getEntityFactory(pocketlistsAttachment::class);
+            /** @var pocketlistsAttachment $attachment */
+            $attachment = $attachmentFactory->createNew();
+            $attachment
+                ->setFilename($uploadedFile->getName())
+                ->setFiletype($type)
+                ->setItemId($item->getId());
+
+            $attachmentFactory->insert($attachment);
+
+            $this->logService->add(
+                $this->logService->getFactory()->createNewAttachmentLog(
+                    (new pocketlistsLogContext())
+                        ->setItem($item)
+                        ->setAttachment($attachment)
+                )
+            );
+
+            $this->errors = [];
+
+            $return = ['id' => $attachment->getId()];
+        }
+
+        return array_merge(
+            [
+                'url' => $uploadedFile->getUrl(),
+                'name' => $uploadedFile->getName(),
+                'type' => $uploadedFile->getType(),
+                'size' => $uploadedFile->getSize(),
+                'id' => -1,
+            ],
+            $return
+        );
     }
 
     /**
