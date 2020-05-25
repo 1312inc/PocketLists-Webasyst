28bb542483cc43bc6015c7aef9863d508694f6b6
vofka <vofka@webasyst.com>
2020-05-25 23:51:36 +0300

    pl2pro hotfix version up 1.2.2

--- plugins/pro/lib/config/plugin.php
+++ plugins/pro/lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array(
     'name'     => 'Pocket Lists PRO',
     'img'      => 'img/pl2pro.png',
-    'version'  => '1.2.1',
+    'version'  => '1.2.2',
     'vendor'   => '1021997',
     'handlers' =>
         array(
