cefc88881374d84cebce13232fbbec896f4a9231
vofka <vofka@webasyst.com>
2020-11-26 10:29:07 +0300

    minor loc & ui fixes

--- lib/actions/backend/pocketlistsBackendLoc.action.php
+++ lib/actions/backend/pocketlistsBackendLoc.action.php
@@ -30,7 +30,7 @@ class pocketlistsBackendLocAction extends pocketlistsViewAction
                      'Assigned to',
                      "Archiving the list will temporarily make this list and all it's items invisible. When you need this list in the future, simply unarchive it and continue from just where you stopped. Archive the list?",
                      'You are about to leave this page without saving your input. Are you sure?',
-                     'Find order by ID',
+                     'Find order or task by ID',
                      'About',
                      'We wish we could implement new features that easy :)'
                  ) as $s) {
