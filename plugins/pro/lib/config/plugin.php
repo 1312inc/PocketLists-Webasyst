9c6a4ce6e97dbdf57b8615c168fe1817741b19de
vofka <vofka@webasyst.com>
2021-07-20 14:52:57 +0300

    multiple wa2 ui tweaks & fixes

--- plugins/pro/lib/config/plugin.php
+++ plugins/pro/lib/config/plugin.php
@@ -2,7 +2,7 @@
 return array(
     'name'     => 'Pocket Lists PRO',
     'img'      => 'img/pl2pro.png',
-    'version'  => '1.2.5',
+    'version'  => '1.2.6',
     'vendor'   => '1021997',
     'handlers' =>
         array(
