908f4c93d0b02d2ce9388fe013d40b2b0b39aaac
vofka <vofka@webasyst.com>
2021-12-29 12:48:21 +0300

    PL version up 4.0.4

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array(
     'name'           => /*_w*/('Pocket Lists'),
     'icon'           => 'img/pocketlists.png',
-    'version'        => '4.0.3',
+    'version'        => '4.0.4',
     'vendor'         => '1021997',
     'frontend'       => true,
     'routing_params' => array(
