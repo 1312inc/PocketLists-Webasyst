fe01971386dd2d65aea81df0059f007c54e2a9e5
moroz <ivan.moroz@webasyst.ru>
2024-05-22 12:17:50 +0300

    #107.26: pocket lists API, pocketlists.list.add

--- api/v1/pocketlists.list.add.method.php
+++ api/v1/pocketlists.list.add.method.php
@@ -16,15 +16,17 @@ class pocketlistsListAddMethod extends pocketlistsApiAbstractMethod
         } elseif ($pocket_id < 1) {
             throw new waAPIException('not_found', _w('Pocket not found'), 404);
         } elseif (!in_array($type, ['checklist', 'notes'])) {
-            throw new waAPIException('unknown_value', 'Unknown type', 400);
+            throw new waAPIException('unknown_value', _w('Unknown type'), 400);
         }
 
         $list_factory = pl2()->getEntityFactory(pocketlistsList::class);
 
         /** @var pocketlistsPocket $pocket */
-        $pocket = $list_factory->findById($pocket_id);
+        $pocket = pl2()->getEntityFactory(pocketlistsPocket::class)->findById($pocket_id);
         if (empty($pocket)) {
             throw new waAPIException('not_found', _w('Pocket not found'), 404);
+        } elseif (pocketlistsRBAC::contactHasAccessToPocket($pocket) != pocketlistsRBAC::RIGHT_ADMIN) {
+            throw new waAPIException('forbidden', _w('Access denied'), 403);
         }
 
         /** @var pocketlistsList $list */
