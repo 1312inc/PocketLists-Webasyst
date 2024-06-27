6e9be2c6959fe35f4515f0c30d71e4fdd862cda8
moroz <ivan.moroz@webasyst.ru>
2024-06-27 14:58:15 +0300

    #107.26: pocket lists API, pocketlists.item.get

--- lib/class/Api/pocketlistsApiAbstractMethod.class.php
+++ lib/class/Api/pocketlistsApiAbstractMethod.class.php
@@ -37,4 +37,58 @@ abstract class pocketlistsApiAbstractMethod extends waAPIMethod
     {
         return pl2()->getUser();
     }
+
+    /**
+     * @param $data
+     * @param array $fields
+     * @param array $field_types
+     * @return array
+     */
+    protected function filterFields($data, array $fields, array $field_types = [])
+    {
+        if (!empty($data) && is_array($data)) {
+            return array_map(function ($el) use ($fields, $field_types) {
+                $res = [];
+                foreach (array_keys($el) as $key) {
+                    if (in_array($key, $fields)) {
+                        if (!isset($field_types[$key]) || $el[$key] === null) {
+                            $res[$key] = $el[$key];
+                            continue;
+                        }
+                        if ($field_types[$key] === 'int') {
+                            $res[$key] = intval($el[$key]);
+                        } elseif ($field_types[$key] === 'bool') {
+                            $res[$key] = boolval($el[$key]);
+                        } elseif ($field_types[$key] === 'float') {
+                            $res[$key] = floatval($el[$key]);
+                        } elseif ($field_types[$key] === 'double') {
+                            $res[$key] = doubleval($el[$key]);
+                        } elseif ($field_types[$key] === 'datetime') {
+                            $res[$key] = $this->formatDatetimeToISO8601($el[$key]);
+                        } else {
+                            $res[$key] = $el[$key];
+                        }
+                    }
+                }
+                return $res;
+            }, array_values($data));
+        }
+
+        return [];
+    }
+
+    /**
+     * @param $sql_dt
+     * @return string
+     */
+    protected function formatDatetimeToISO8601($sql_dt)
+    {
+        try {
+            $dt = new DateTime((string) $sql_dt);
+            $dt->setTimezone(new DateTimeZone('UTC'));
+            return $dt->format('Y-m-d\TH:i:s.u\Z');
+        } catch (Exception $ex) {
+            return $sql_dt;
+        }
+    }
 }
