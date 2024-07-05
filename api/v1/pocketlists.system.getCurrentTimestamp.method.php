9895b0646502b1e87968cb7a7f4c688e2c1c5d36
moroz <ivan.moroz@webasyst.ru>
2024-07-05 16:16:50 +0300

    #107.26: pocket lists API, cosmetic

--- api/v1/pocketlists.system.getCurrentTimestamp.method.php
+++ api/v1/pocketlists.system.getCurrentTimestamp.method.php
@@ -2,8 +2,6 @@
 
 class pocketlistsSystemGetCurrentTimestampMethod extends pocketlistsApiAbstractMethod
 {
-    protected $method = self::METHOD_GET;
-
     public function execute()
     {
         $this->response = time();
