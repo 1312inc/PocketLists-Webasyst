863889c80a1cbe8b49682ec5ce9502873f429092
moroz <ivan.moroz@webasyst.ru>
2024-08-20 19:18:44 +0300

    #107.54: PL API, pocketlists.item.update / multiple additions

--- api/v1/pocketlists.item.update.method.php
+++ api/v1/pocketlists.item.update.method.php
@@ -6,190 +6,216 @@ class pocketlistsItemUpdateMethod extends pocketlistsApiAbstractMethod
 
     public function execute()
     {
-        $_json = $this->readBodyAsJson();
-        $item_id = ifset($_json, 'id', 0);
-        $list_id = ifset($_json, 'list_id', 0);
-        $name = ifset($_json, 'name', '');
-        $note = ifset($_json, 'note', '');
-        $sort = ifset($_json, 'sort', '0');
-        $assigned_contact_id = ifset($_json, 'assigned_contact_id', 0);
-        $priority = ifset($_json, 'priority', 0);
-        $due_datetime = ifset($_json, 'due_datetime', '0');
-        $attachments = ifset($_json, 'attachments', null);
-
-        /** @var pocketlistsItem $item */
-        if (!is_numeric($item_id)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'item_id'), 400);
-        } elseif (!is_numeric($list_id)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'list_id'), 400);
-        } elseif (!is_numeric($assigned_contact_id)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'assigned_contact_id'), 400);
-        } elseif (!is_numeric($priority)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'priority'), 400);
-        } elseif (!is_string($name)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'name'), 400);
-        } elseif (!is_string($note)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'note'), 400);
-        } elseif (!is_string($sort)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'sort'), 400);
-        } elseif (!is_string($due_datetime)) {
-            throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'due_datetime'), 400);
-        } elseif (empty($item_id)) {
-            throw new waAPIException('required_param', sprintf_wp('Missing required parameter: “%s”.', 'id'), 400);
-        } elseif (empty($list_id)) {
-            throw new waAPIException('required_param', sprintf_wp('Missing required parameter: “%s”.', 'list_id'), 400);
-        } elseif ($item_id < 1 || !$item = pl2()->getEntityFactory(pocketlistsItem::class)->findById($item_id)) {
-            throw new waAPIException('not_found', _w('Item not found'), 404);
-        } elseif ($list_id < 1 || !$list = pl2()->getEntityFactory(pocketlistsList::class)->findById($list_id)) {
-            throw new waAPIException('not_found', _w('List not found'), 404);
+        $items = $this->readBodyAsJson();
+        if (empty($items)) {
+            throw new waAPIException('required_param', _w('Missing data'), 400);
+        } elseif (!is_array($items)) {
+            throw new waAPIException('type_error', _w('Type error data'), 400);
         }
 
-        if ($item->getListId()) {
-            if (!pocketlistsRBAC::canAccessToList($item->getList())) {
-                throw new waAPIException('forbidden', _w('Access denied'), 403);
-            }
-            if (
-                $item->getAssignedContactId()
-                && !pocketlistsRBAC::canAccessToList($item->getList(), $item->getAssignedContactId())
-            ) {
-                $item->setAssignedContactId();
+        $items_in_db = [];
+        $assign_contacts = [];
+        $list_id_available = [];
+        $user_id = $this->getUser()->getId();
+        $item_ids = array_unique(array_column($items, 'id'));
+        $list_ids = array_unique(array_column($items, 'list_id'));
+        $assigned_contact_ids = array_unique(array_column($items, 'assigned_contact_id'));
+
+        if (!empty($item_ids)) {
+            /** @var pocketlistsItemModel $item_model */
+            $item_model = pl2()->getModel(pocketlistsItem::class);
+            $items_in_db = $item_model->select('*')->where('id IN (:item_ids)', ['item_ids' => $item_ids])->fetchAll('id');
+            $list_id_available = pocketlistsRBAC::getAccessListForContact($user_id);
+        }
+        if (!empty($list_ids)) {
+            /** @var pocketlistsListModel $list_model */
+            $list_model = pl2()->getModel(pocketlistsList::class);
+            $list_ids = $list_model->select('id')->where('id IN (:list_ids)', ['list_ids' => $list_ids])->fetchAll(null, true);
+        }
+        if (!empty($assigned_contact_ids)) {
+            /** @var pocketlistsContact $_assign_contact */
+            foreach (pl2()->getEntityFactory(pocketlistsContact::class)->createNewWithIds($assigned_contact_ids) as $_assign_contact) {
+                if ($_assign_contact->isExists()) {
+                    $assign_contacts[$_assign_contact->getId()] = $_assign_contact;
+                }
             }
         }
 
-        $match_note = pocketlistsNaturalInput::matchNote($name);
-        if ($match_note) {
-            if (empty($note)) {
-                $name = $match_note['name'];
-                $note = $match_note['note'];
+        /** validate */
+        foreach ($items as &$_item) {
+            /** set default */
+            $_item = [
+                'id'                  => ifset($_item, 'id', null),
+                'list_id'             => ifset($_item, 'list_id', null),
+                'contact_id'          => $user_id,
+                'parent_id'           => 0,
+                'sort'                => ifset($_item, 'sort', '0'),
+                'has_children'        => 0,
+                'status'              => 0,
+                'priority'            => ifset($_item, 'priority', 0),
+                'calc_priority'       => 0,
+                'create_datetime'     => date('Y-m-d H:i:s'),
+                'update_datetime'     => null,
+                'complete_datetime'   => null,
+                'complete_contact_id' => null,
+                'name'                => ifset($_item, 'name', ''),
+                'note'                => ifset($_item, 'note', ''),
+                'due_date'            => null,
+                'due_datetime'        => ifset($_item, 'due_datetime', null),
+                'location_id'         => null,
+                'amount'              => 0,
+                'currency_iso3'       => null,
+                'assigned_contact_id' => ifset($_item, 'assigned_contact_id', null),
+                'repeat'              => 0,
+                'key_list_id'         => null,
+                'uuid'                => ifset($_item, 'uuid', null),
+                'attachments'         => ifset($_item, 'attachments', []),
+                'errors'              => [],
+                'status_code'         => null,
+            ];
+
+            if (empty($_item['id'])) {
+                $_item['errors'][] = sprintf_wp('Missing required parameter: “%s”.', 'id');
+            } elseif (!is_numeric($_item['id'])) {
+                $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'item_id');
+            } elseif (!array_key_exists($_item['id'], $items_in_db)) {
+                $_item['errors'][] = _w('Item not found');
             }
-        }
-        if ($assigned_contact_id) {
-            $assign_contact = pl2()->getEntityFactory(pocketlistsContact::class)->createNewWithId($assigned_contact_id);
-            if ($assign_contact->isExists()) {
-                $item->setAssignedContact($assign_contact);
+
+            if ($_item['list_id']) {
+                if (!is_numeric($_item['list_id'])) {
+                    $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'list_id');
+                } elseif (!in_array($_item['list_id'], $list_ids)) {
+                    $_item['errors'][] = _w('List not found');
+                } elseif (!in_array($_item['list_id'], $list_id_available)) {
+                    $_item['errors'][] = _w('Access denied');
+                }
+            }
+
+            if ($_item['assigned_contact_id']) {
+                if (!is_numeric($_item['assigned_contact_id'])) {
+                    $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'assigned_contact_id');
+                } elseif (!array_key_exists($_item['assigned_contact_id'], $assign_contacts)) {
+                    $_item['errors'][] = _w('Assigned contact not found');
+                }
+            }
+
+            if ($_item['priority']) {
+                if (!is_numeric($_item['priority'])) {
+                    $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'priority');
+                } elseif (!in_array($_item['priority'], [1, 2, 3, 4, 5])) {
+                    $_item['errors'][] = _w('Unknown value priority');
+                }
+            }
+
+            if (!is_string($_item['name'])) {
+                $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'name');
+            }
+
+            if (!is_string($_item['note'])) {
+                $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'note');
+            }
+
+            if (!is_string($_item['sort'])) {
+                $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'sort');
+            }
+
+            if ($_item['due_datetime']) {
+                if (!is_string($_item['due_datetime'])) {
+                    $_item['errors'][] = sprintf_wp('Type error parameter: “%s”.', 'due_datetime');
+                } else {
+                    $dt = date_create($_item['due_datetime']);
+                    if ($dt) {
+                        $_item['due_date'] = $dt->format('Y-m-d');
+                        $_item['due_datetime'] = $dt->format('Y-m-d H:i:s');
+                    } else {
+                        $_item['errors'][] = _w('Unknown value due_datetime');
+                    }
+                }
+            }
+
+            if (empty($_item['errors'])) {
+                $_item += $items_in_db[$_item['id']];
+                unset($_item['errors']);
             } else {
-                throw new waAPIException('not_found', _w('Assigned contact not found'), 404);
-            }
-        }
-        if ($priority) {
-            if (!in_array($priority, [1, 2, 3, 4, 5])) {
-                throw new waAPIException('unknown_value', _w('Unknown priority'), 400);
-            }
-        } else {
-            $match_priority = pocketlistsNaturalInput::matchPriority($name);
-            if ($match_priority) {
-                $name = $match_priority['name'];
-                $priority = (int) $match_priority['priority'];
-            }
-        }
-        if (!empty($due_datetime)) {
-            $dt = date_create($due_datetime);
-            if ($dt) {
-                $item->setDueDate($dt->format('Y-m-d'))
-                    ->setDueDatetime($dt->format('Y-m-d H:i:s'));
-            } else {
-                throw new waAPIException('unknown_value', _w('Unknown date'), 400);
-            }
-        }
-        if (!is_null($attachments)) {
-            if (empty($attachments)) {
-                /** @var pocketlistsAttachmentFactory $attachment_factory */
-                $attachment_factory = pl2()->getEntityFactory(pocketlistsAttachment::class);
-                $attachment_factory->deleteAllByItem($item);
-                $item->setAttachmentsCount(0);
-            } else {
-                $attachments = $this->updateFiles($item_id, $attachments);
-                $item->setAttachmentsCount($item->getAttachmentsCount() + count(array_column($attachments, 'id')));
+                $_item['attachments'] = [];
+                $_item['status_code'] = 'error';
             }
         }
 
-        $item->setName($name)
-            ->setNote($note)
-            ->setSort($sort)
-            ->setPriority($priority)
-            ->setAssignedContactId($assigned_contact_id)
-            ->setUpdateDatetime(date('Y-m-d H:i:s'));
-
-        /** @var pocketlistsItemFactory $item_factory */
-        $item_factory = pl2()->getEntityFactory(pocketlistsItem::class);
-        $saved = $item_factory->save($item);
-        if (!$saved) {
-            throw new waAPIException('save_error', _w('Error while saving item'));
+        $items_ok = array_filter($items, function ($i) {
+            return is_null($i['status_code']);
+        });
+        $items_err = array_diff_key($items, $items_ok);
+        if (!empty($items_ok)) {
+            $item_model = pl2()->getModel(pocketlistsItem::class);
+            try {
+                foreach ($items_ok as &$_items_ok) {
+                    $result = $item_model->updateById($_items_ok['id'], $_items_ok);
+                    if ($result) {
+                        $_items_ok['status_code'] = 'ok';
+                        if (!empty($_items_ok['attachments'])) {
+                            $_items_ok['attachments'] = $this->updateFiles($_items_ok['id'], $_items_ok['attachments']);
+                        }
+                    } else {
+                        $_items_ok['status_code'] = 'error';
+                        $_items_ok['errors'][] = _w('Failed to update');
+                    }
+                }
+            } catch (Exception $ex) {
+                throw new waAPIException('error', sprintf_wp('Error on transaction import save: %s', $ex->getMessage()), 400);
+            }
         }
 
-        $result = $this->filterFields([[
-            'id'                    => $item->getId(),
-            'list_id'               => $item->getListId(),
-            'contact_id'            => $item->getContactId(),
-            'parent_id'             => $item->getParentId(),
-            'sort'                  => $item->getSort(),
-            'has_children'          => $item->isHasChildren(),
-            'status'                => $item->getStatus(),
-            'priority'              => $item->getPriority(),
-            'calc_priority'         => $item->getCalcPriority(),
-            'create_datetime'       => $item->getCreateDatetime(),
-            'update_datetime'       => $item->getUpdateDatetime(),
-            'complete_datetime'     => $item->getCompleteDatetime(),
-            'complete_contact_id'   => $item->getCompleteContactId(),
-            'name'                  => $item->getName(),
-            'note'                  => $item->getNote(),
-            'due_date'              => $item->getDueDate(),
-            'due_datetime'          => $item->getDueDatetime(),
-            'location_id'           => $item->getLocationId(),
-            'amount'                => $item->getAmount(),
-            'currency_iso3'         => $item->getCurrencyIso3(),
-            'assigned_contact_id'   => $item->getAssignedContactId(),
-            'repeat'                => $item->getRepeat(),
-            'key_list_id'           => $item->getKeyListId(),
-            'uuid'                  => $item->getUuid(),
-            'attachments'           => $attachments
-        ]], [
-            'id',
-            'list_id',
-            'contact_id',
-            'parent_id',
-            'sort',
-            'has_children',
-            'status',
-            'priority',
-            'calc_priority',
-            'create_datetime',
-            'update_datetime',
-            'complete_datetime',
-            'complete_contact_id',
-            'name',
-            'note',
-            'due_date',
-            'due_datetime',
-            'location_id',
-            'amount',
-            'currency_iso3',
-            'assigned_contact_id',
-            'repeat',
-            'key_list_id',
-            'uuid',
-            'attachments'
-        ], [
-            'id' => 'int',
-            'list_id' => 'int',
-            'contact_id' => 'int',
-            'parent_id' => 'int',
-            'has_children' => 'int',
-            'status' => 'int',
-            'priority' => 'int',
-            'calc_priority' => 'int',
-            'create_datetime' => 'datetime',
-            'update_datetime' => 'datetime',
-            'complete_datetime' => 'datetime',
-            'complete_contact_id' => 'int',
-            'due_datetime' => 'datetime',
-            'location_id' => 'int',
-            'amount' => 'float',
-            'assigned_contact_id' => 'int',
-            'repeat' => 'int',
-            'key_list_id' => 'int'
+        $this->response = $this->filterFields(
+            array_merge($items_ok, $items_err),
+            [
+                'id',
+                'list_id',
+                'contact_id',
+                'parent_id',
+                'sort',
+                'has_children',
+                'status',
+                'priority',
+                'calc_priority',
+                'create_datetime',
+                'update_datetime',
+                'complete_datetime',
+                'complete_contact_id',
+                'name',
+                'note',
+                'due_date',
+                'due_datetime',
+                'location_id',
+                'amount',
+                'currency_iso3',
+                'assigned_contact_id',
+                'repeat',
+                'key_list_id',
+                'uuid',
+                'attachments',
+                'errors',
+                'status_code'
+            ], [
+                'id' => 'int',
+                'list_id' => 'int',
+                'contact_id' => 'int',
+                'parent_id' => 'int',
+                'has_children' => 'int',
+                'status' => 'int',
+                'priority' => 'int',
+                'calc_priority' => 'int',
+                'create_datetime' => 'datetime',
+                'update_datetime' => 'datetime',
+                'complete_datetime' => 'datetime',
+                'complete_contact_id' => 'int',
+                'due_datetime' => 'datetime',
+                'location_id' => 'int',
+                'amount' => 'float',
+                'assigned_contact_id' => 'int',
+                'repeat' => 'int',
+                'key_list_id' => 'int'
         ]);
-
-        $this->response = reset($result);
     }
 }
