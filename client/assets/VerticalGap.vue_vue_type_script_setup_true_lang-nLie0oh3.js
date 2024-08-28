aa3d95cd05da453d9defdd2b2348c03213d246bc
Konstantin Zaytsev <zaytsev.const@gmail.com>
2024-08-28 15:03:40 +0300

    107.52: Implement linked list ordering by `prev_item_id` field

--- client/assets/VerticalGap.vue_vue_type_script_setup_true_lang-nLie0oh3.js
+++
@@ -1,1 +0,0 @@
-import{a as s,b as a,s as e,E as o,o as r}from"./index-v6l8a5yj.js";const c=s({__name:"VerticalGap",props:{space:{}},setup(t){return(p,l)=>(r(),a("div",{class:o(["tw-flex tw-flex-col",{"tw-gap-2":p.$props.space===2,"tw-gap-4":p.$props.space===4,"tw-gap-6":p.$props.space===6,"tw-gap-8":p.$props.space===8}])},[e(p.$slots,"default")],2))}});export{c as _};
