8331318f4dc63d9ff6bf56d06bd83b8a66169f22
moroz <ivan.moroz@webasyst.ru>
2024-09-12 16:27:21 +0300

    #107.62: PL, log: single -> multiple

--- lib/class/pocketlistsLogService.class.php
+++ lib/class/pocketlistsLogService.class.php
@@ -6,6 +6,26 @@
  */
 class pocketlistsLogService
 {
+    const ENTITIES = [
+        pocketlistsLog::ENTITY_USER,
+        pocketlistsLog::ENTITY_POCKET,
+        pocketlistsLog::ENTITY_LIST,
+        pocketlistsLog::ENTITY_ITEM,
+        pocketlistsLog::ENTITY_COMMENT,
+        pocketlistsLog::ENTITY_ATTACHMENT
+    ];
+    const ACTIONS = [
+        pocketlistsLog::ACTION_ADD,
+        pocketlistsLog::ACTION_EDIT,
+        pocketlistsLog::ACTION_UPDATE,
+        pocketlistsLog::ACTION_DELETE,
+        pocketlistsLog::ACTION_ASSIGN,
+        pocketlistsLog::ACTION_ARCHIVE,
+        pocketlistsLog::ACTION_UNARCHIVE,
+        pocketlistsLog::ACTION_COMPLETE,
+        pocketlistsLog::ACTION_UNCOMPLETE
+    ];
+
     /**
      * @var pocketlistsLogFactory
      */
@@ -46,24 +66,82 @@ class pocketlistsLogService
                     $log
                 )
             );
-
-            pocketlistsWebSoket::getInstance()->sendWebsocketData([
-                'client'     => waRequest::server('HTTP_X_PL_API_CLIENT', ''),
-                'id'                  => $log->getId(),
-                'action'              => $log->getAction(),
-                'entity_type'         => $log->getEntityType(),
-                'contact_id'          => $log->getContactId(),
-                'pocket_id'           => $log->getPocketId(),
-                'list_id'             => $log->getListId(),
-                'item_id'             => $log->getItemId(),
-                'comment_id'          => $log->getCommentId(),
-                'attachment_id'       => $log->getAttachmentId(),
-                'assigned_contact_id' => $log->getAssignedContactId(),
-                'params'              => $log->getParams()
-            ]);
         }
 
         return $ok;
+    }
+
+    /**
+     * @param $entity
+     * @param $action
+     * @param $logs
+     * @return bool
+     * @throws waException
+     */
+    public static function multipleAdd($entity, $action, $logs = [])
+    {
+        if (
+            empty($logs)
+            || !is_array($logs)
+            || !in_array($entity, self::ENTITIES)
+            || !in_array($action, self::ACTIONS)
+        ) {
+            return false;
+        }
+
+        $default = [
+            'action'              => $action,
+            'entity_type'         => $entity,
+            'contact_id'          => pl2()->getUser()->getId(),
+            'pocket_id'           => null,
+            'list_id'             => null,
+            'item_id'             => null,
+            'comment_id'          => null,
+            'attachment_id'       => null,
+            'location_id'         => null,
+            'additional_id'       => null,
+            'assigned_contact_id' => null,
+            'params'              => '',
+            'create_datetime'     => date('Y-m-d H:i:s')
+        ];
+
+        foreach ($logs as &$_log) {
+            $id = ifset($_log, 'id', null);
+            unset($_log['id'], $_log['action']);
+            $params = [
+                $entity => array_filter(array_diff_key($_log, $default), function ($l) {return !(is_null($l) || $l === []);})
+            ];
+            $_log = array_intersect_key($_log, $default) + $default;
+            $_log['params'] = json_encode($params, JSON_UNESCAPED_UNICODE);
+            if ($id) {
+                $_log[$entity.'_id'] = $id;
+            }
+        }
+        unset($_log);
+
+        $log_model = pl2()->getModel(pocketlistsLog::class);
+        $result = $log_model->multipleInsert($logs);
+        if ($result->getResult()) {
+            $last_id = $result->lastInsertId();
+            $rows_count = $result->affectedRows();
+            if ($rows_count === count($logs)) {
+                foreach ($logs as &$log) {
+                    $log['id'] = $last_id++;
+                    pocketlistsWebSoket::getInstance()->sendWebsocketData(
+                        $log + ['client' => waRequest::server('HTTP_X_PL_API_CLIENT', '')]
+                    );
+                }
+                pl2()->getEventDispatcher()->dispatch(
+                    new pocketlistsEvent(
+                        pocketlistsEventStorage::LOGS_INSERT,
+                        $logs
+                    )
+                );
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
