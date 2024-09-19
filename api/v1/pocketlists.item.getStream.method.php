7b6ca73d0b34cd3a68894c56a8ef3a0c0cf4c756
moroz <ivan.moroz@webasyst.ru>
2024-09-19 18:48:28 +0300

    #107.83: PL API, new pocketlists.item.getStream

---
+++ api/v1/pocketlists.item.getStream.method.php
@@ -0,0 +1,164 @@
+<?php
+
+class pocketlistsItemGetStreamMethod extends pocketlistsApiAbstractMethod
+{
+    public function execute()
+    {
+        $filter = $this->get('filter', true);
+        $limit  = $this->get('limit');
+        $offset = $this->get('offset');
+
+        if (isset($limit)) {
+            if (!is_numeric($limit)) {
+                throw new waAPIException('error_type', sprintf_wp('Invalid type %s', 'limit'), 400);
+            } elseif ($limit < 1) {
+                throw new waAPIException('negative_value', _w('The parameter has a negative value'), 400);
+            }
+            $limit = (int) min($limit, self::MAX_LIMIT);
+        } else {
+            $limit = self::DEFAULT_LIMIT;
+        }
+        if (isset($offset)) {
+            if (!is_numeric($offset)) {
+                throw new waAPIException('error_type', sprintf_wp('Invalid type %s', 'offset'), 400);
+            } elseif ($offset < 0) {
+                throw new waAPIException('negative_value', _w('The parameter has a negative value'), 400);
+            }
+            $offset = intval($offset);
+        } else {
+            $offset = 0;
+        }
+
+        if (!is_string($filter)) {
+            throw new waAPIException('error_type', sprintf_wp('Invalid type %s', 'filter'), 400);
+        }
+
+        $filter = trim(trim($filter), '/');
+        if ($filter === '') {
+            throw new waAPIException('required_param', sprintf_wp('Missing required parameter: “%s”.', 'filter'), 400);
+        }
+
+        list($items, $total_count) = $this->getItems($filter, $limit, $offset);
+
+        $this->response = [
+            'filter' => "/$filter",
+            'offset' => $offset,
+            'limit'  => $limit,
+            'count'  => $total_count,
+            'data'   => $this->filterFields(
+                $items,
+                [
+                    'id',
+                    'list_id',
+                    'contact_id',
+                    'parent_id',
+                    'sort',
+                    'rank',
+                    'has_children',
+                    'status',
+                    'priority',
+                    'calc_priority',
+                    'create_datetime',
+                    'update_datetime',
+                    'complete_datetime',
+                    'complete_contact_id',
+                    'name',
+                    'note',
+                    'due_date',
+                    'due_datetime',
+                    'location_id',
+                    'amount',
+                    'currency_iso3',
+                    'assigned_contact_id',
+                    'repeat',
+                    'key_list_id',
+                    'uuid',
+                    'attachments'
+                ],
+                [
+                    'id' => 'int',
+                    'list_id' => 'int',
+                    'contact_id' => 'int',
+                    'parent_id' => 'int',
+                    'sort' => 'int',
+                    'has_children' => 'int',
+                    'status' => 'int',
+                    'priority' => 'int',
+                    'calc_priority' => 'int',
+                    'create_datetime' => 'datetime',
+                    'update_datetime' => 'datetime',
+                    'complete_datetime' => 'datetime',
+                    'complete_contact_id' => 'int',
+                    'due_datetime' => 'datetime',
+                    'location_id' => 'int',
+                    'amount' => 'float',
+                    'assigned_contact_id' => 'int',
+                    'repeat' => 'int',
+                    'key_list_id' => 'int'
+                ]
+            )
+        ];
+    }
+
+    private function getItems($filter, $limit, $offset)
+    {
+        $available_filters = ['todos'];
+        $available_subfilters = [
+            'upnext',
+            'priority',
+            'user',
+            'search'
+        ];
+        $filter_split = explode('/', $filter);
+        if (
+            !in_array(count($filter_split), [2, 3])
+            || !in_array($filter_split[0], $available_filters)
+            || !in_array($filter_split[1], $available_subfilters)
+        ) {
+            throw new waAPIException('unknown_value', _w('Unknown filter value'), 400);
+        }
+
+        $where = '1 = 1';
+        $sort  = '1 = 1';
+        $plim  = pl2()->getModel(pocketlistsItem::class);
+        switch ($filter_split[1]) {
+            case 'upnext':
+                /** /todos/upnext */
+                $sort = 'i.calc_priority DESC, i.due_date ASC';
+                break;
+            case 'priority':
+                /** /todos/priority */
+                $sort = 'i.priority DESC';
+                break;
+            case 'user':
+                /** todos/user/ID */
+                if (
+                    !isset($filter_split[2])
+                    || !is_numeric($filter_split[2])
+                    || $filter_split[2] < 1
+                    || $filter_split[2] != (int) $filter_split[2]
+                ) {
+                    throw new waAPIException('unknown_value', _w('Unknown user'), 400);
+                }
+                $where .= ' AND assigned_contact_id = '.(int) $filter_split[2];
+                break;
+            case 'search':
+                /** todos/search/KEYWORD */
+                $where .= " AND i.name LIKE '%".$plim->escape($filter_split[2])."%'";
+                break;
+        }
+
+        $sql = $plim->getQuery(true);
+        $items = $plim->query(
+            "$sql WHERE $where ORDER BY $sort LIMIT i:offset, i:limit", [
+            'contact_id' => $this->getUser()->getId(),
+            'limit'      => $limit,
+            'offset'     => $offset
+        ])->fetchAll('id');
+
+        return [
+            $items,
+            (int) $plim->query('SELECT FOUND_ROWS()')->fetchField()
+        ];
+    }
+}
