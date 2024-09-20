78ff578be2bbf5a9d31946b60a9b9778a0ee9327
moroz <ivan.moroz@webasyst.ru>
2024-09-20 18:25:30 +0300

    #107.89: PL, gorshochek cook multi-user, settings

--- plugins/gorshochek/lib/actions/settings/pocketlistsGorshochekPluginSettingsCook.action.php
+++ plugins/gorshochek/lib/actions/settings/pocketlistsGorshochekPluginSettingsCook.action.php
@@ -6,5 +6,13 @@ class pocketlistsGorshochekPluginSettingsCookAction extends waViewAction
 
     public function execute()
     {
+        /** @var pocketlistsContactFactory $contact_factory */
+        $contact_factory = pl2()->getEntityFactory(pocketlistsContact::class);
+        $teammates = $contact_factory->getTeammates(pocketlistsRBAC::getAccessContacts(), true, true, true);
+
+        $this->view->assign([
+            'curr_user' => wa()->getUser(),
+            'all_users' => $teammates
+        ]);
     }
 }
