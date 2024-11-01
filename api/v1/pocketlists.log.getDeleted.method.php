577a19f74c0a954d339965603efa9e3d49221416
moroz <ivan.moroz@webasyst.ru>
2024-11-01 17:34:58 +0300

    #107.130 PL API, pocketlists.log.* Standard Response Structure fix

--- api/v1/pocketlists.log.getDeleted.method.php
+++ api/v1/pocketlists.log.getDeleted.method.php
@@ -9,9 +9,21 @@ class pocketlistsLogGetDeletedMethod extends pocketlistsApiAbstractMethod
 
         if (isset($limit)) {
             if (!is_numeric($limit)) {
-                throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'limit'), 400);
+                $this->http_status_code = 400;
+                $this->response = [
+                    'status_code' => 'error',
+                    'error'       => sprintf_wp('Type error parameter: “%s”.', 'limit'),
+                    'data'        => []
+                ];
+                return;
             } elseif ($limit < 1) {
-                throw new waAPIException('negative_value', _w('The parameter has a negative value'), 400);
+                $this->http_status_code = 400;
+                $this->response = [
+                    'status_code' => 'error',
+                    'error'       => _w('The parameter has a negative value'),
+                    'data'        => []
+                ];
+                return;
             }
             $limit = (int) min($limit, self::MAX_LIMIT);
         } else {
@@ -19,9 +31,21 @@ class pocketlistsLogGetDeletedMethod extends pocketlistsApiAbstractMethod
         }
         if (isset($offset)) {
             if (!is_numeric($offset)) {
-                throw new waAPIException('type_error', sprintf_wp('Type error parameter: “%s”.', 'offset'), 400);
+                $this->http_status_code = 400;
+                $this->response = [
+                    'status_code' => 'error',
+                    'error'       => sprintf_wp('Type error parameter: “%s”.', 'offset'),
+                    'data'        => []
+                ];
+                return;
             } elseif ($offset < 1) {
-                throw new waAPIException('negative_value', _w('The parameter has a negative value'), 400);
+                $this->http_status_code = 400;
+                $this->response = [
+                    'status_code' => 'error',
+                    'error'       => _w('The parameter has a negative value'),
+                    'data'        => []
+                ];
+                return;
             }
         } else {
             $offset = 0;
@@ -36,41 +60,41 @@ class pocketlistsLogGetDeletedMethod extends pocketlistsApiAbstractMethod
         )->fetchAll();
         $total_count = (int) $log_model->query('SELECT FOUND_ROWS()')->fetchField();
 
-        $this->response = [
+        $this->response['meta'] = [
             'offset' => $offset,
             'limit'  => $limit,
-            'count'  => $total_count,
-            'data'   => $this->filterFields(
-                $logs,
-                [
-                    'id',
-                    'action',
-                    'entity_type',
-                    'contact_id',
-                    'pocket_id',
-                    'list_id',
-                    'item_id',
-                    'comment_id',
-                    'attachment_id',
-                    'location_id',
-                    'additional_id',
-                    'assigned_contact_id',
-                    'create_datetime',
-                    'params'
-                ], [
-                    'id' => 'int',
-                    'contact_id' => 'int',
-                    'pocket_id' => 'int',
-                    'list_id' => 'int',
-                    'item_id' => 'int',
-                    'comment_id' => 'int',
-                    'attachment_id' => 'int',
-                    'location_id' => 'int',
-                    'additional_id' => 'int',
-                    'assigned_contact_id' => 'int',
-                    'create_datetime' => 'datetime'
-                ]
-            )
+            'count'  => $total_count
         ];
+        $this->response['data'] = $this->responseListWrapper(
+            $logs,
+            [
+                'id',
+                'action',
+                'entity_type',
+                'contact_id',
+                'pocket_id',
+                'list_id',
+                'item_id',
+                'comment_id',
+                'attachment_id',
+                'location_id',
+                'additional_id',
+                'assigned_contact_id',
+                'create_datetime',
+                'params'
+            ], [
+                'id' => 'int',
+                'contact_id' => 'int',
+                'pocket_id' => 'int',
+                'list_id' => 'int',
+                'item_id' => 'int',
+                'comment_id' => 'int',
+                'attachment_id' => 'int',
+                'location_id' => 'int',
+                'additional_id' => 'int',
+                'assigned_contact_id' => 'int',
+                'create_datetime' => 'datetime'
+            ]
+        );
     }
 }
