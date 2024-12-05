69a3cac3e26e3925d1615fac145e38b471d3c5d2
moroz <ivan.moroz@webasyst.ru>
2024-12-05 13:16:05 +0300

    #107.191 PL API, pocketlists.pockets.get add list count

--- api/v1/pocketlists.pockets.get.method.php
+++ api/v1/pocketlists.pockets.get.method.php
@@ -7,18 +7,19 @@ class pocketlistsPocketsGetMethod extends pocketlistsApiAbstractMethod
         $ids = $this->get('id');
         $starting_from = $this->get('starting_from');
 
-        $where = 'id IN (i:access_id)';
-        if (isset($ids)) {
-            if (!is_array($ids)) {
-                throw new pocketlistsApiException(sprintf_wp('Invalid type %s', 'id'), 400);
-            }
-            $ids = array_unique(array_filter($ids, function ($_i) {
-                return is_numeric($_i);
-            }));
-            if (!$ids) {
-                throw new pocketlistsApiException(_w('Pockets not found'), 404);
-            }
-        }
+        $sql_parts = [
+            'select' => [
+                '*' => ' SQL_CALC_FOUND_ROWS *',
+                'lists_count' => '(SELECT count(*) FROM pocketlists_list pl WHERE pl.pocket_id = pp.id) lists_count',
+            ],
+            'from' => ['pp' => 'pocketlists_pocket pp'],
+            'join' => [],
+            'where' => [
+                'and' => ['pp.id IN (i:access_id)']
+            ],
+            'order by' => ['sort, `rank`']
+        ];
+
         if (isset($starting_from)) {
             if (!is_string($starting_from)) {
                 throw new pocketlistsApiException(sprintf_wp('Invalid type %s', 'starting_from'), 400);
@@ -30,18 +31,37 @@ class pocketlistsPocketsGetMethod extends pocketlistsApiAbstractMethod
             } else {
                 throw new pocketlistsApiException(_w('Unknown value starting_from'), 400);
             }
-            $where .= ' AND (update_datetime >= s:starting_from OR create_datetime >= s:starting_from)';
-            $order = 'update_datetime DESC, sort, `rank`';
-        } else {
-            $order = 'sort, `rank`';
+            $sql_parts['where']['and'][] = 'update_datetime >= s:starting_from OR create_datetime >= s:starting_from';
+            $sql_parts['order by'] = ['update_datetime DESC, sort, `rank`'];
         }
 
         $accessed_pockets = pocketlistsRBAC::getAccessPocketForContact($this->getUser()->getId());
+        if (isset($ids)) {
+            if (!is_array($ids)) {
+                throw new pocketlistsApiException(sprintf_wp('Invalid type %s', 'id'), 400);
+            }
+            $ids = array_unique(array_filter($ids, function ($_i) {
+                return is_numeric($_i);
+            }));
+            if (!$ids) {
+                throw new pocketlistsApiException(_w('Pockets not found'), 404);
+            }
+            $accessed_pockets = array_intersect($accessed_pockets, $ids);
+            if (empty($accessed_pockets)) {
+                throw new pocketlistsApiException(_w('Pocket access denied'), 403);
+            }
+        }
+
         $plp = pl2()->getModel(pocketlistsPocket::class);
-        $pockets = $plp->select('*')
-            ->where($where, ['access_id' => $accessed_pockets, 'starting_from' => $starting_from])
-            ->order($order)
-            ->fetchAll();
+        $sql = $plp->buildSqlComponents($sql_parts);
+        $pockets = $plp->query($sql, [
+            'access_id' => $accessed_pockets,
+            'starting_from' => $starting_from
+        ])->fetchAll();
+
+        foreach ($pockets as &$_pocket) {
+            $_pocket['extended_data']['lists_count'] = (int) ifset($_pocket, 'lists_count', 0);
+        }
 
         $this->response['meta'] = [
             'offset' => 0,
@@ -59,7 +79,8 @@ class pocketlistsPocketsGetMethod extends pocketlistsApiAbstractMethod
                 'create_datetime',
                 'update_datetime',
                 'passcode',
-                'uuid'
+                'uuid',
+                'extended_data'
             ], [
                 'id' => 'int',
                 'sort' => 'int',
