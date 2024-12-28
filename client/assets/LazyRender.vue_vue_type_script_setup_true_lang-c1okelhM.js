cdd769dde45ce522cef8e5ca53ab007ab9a1f3fc
Konstantin Zaytsev <zaytsev.const@gmail.com>
2024-12-28 13:52:13 +0300

    107.221: Improve date / time fields UX

--- client/assets/LazyRender.vue_vue_type_script_setup_true_lang-c1okelhM.js
+++ client/assets/LazyRender.vue_vue_type_script_setup_true_lang-c1okelhM.js
@@ -1,4 +1,4 @@
-import{d as Ye,r as me,k as nn,q as Be,K as ht,o as $t,c as Ut,F as on,s as rn,E as Ge,ak as an,I as ln}from"./index-3MI8oU1b.js";let Gt;function sn(){return{draggingEntityName:Gt,setDraggingEntityName:n=>{Gt=n},getDraggingEntityName:()=>Gt,clearDraggingEntityName:()=>{Gt=void 0}}}/**!
+import{d as Ye,r as me,k as nn,q as Be,K as ht,o as $t,c as Ut,F as on,s as rn,E as Ge,as as an,I as ln}from"./index-S31yj8BE.js";let Gt;function sn(){return{draggingEntityName:Gt,setDraggingEntityName:n=>{Gt=n},getDraggingEntityName:()=>Gt,clearDraggingEntityName:()=>{Gt=void 0}}}/**!
  * Sortable 1.15.2
  * @author	RubaXa   <trash@rubaxa.org>
  * @author	owenm    <owen23355@gmail.com>
