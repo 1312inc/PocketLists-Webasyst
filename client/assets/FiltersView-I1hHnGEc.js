f6afd4aec19d4f213eda08875f2e201b0a557e31
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-02-19 19:53:49 +0300

    new list > server response (?) break for UI flow #107.298

---
+++ client/assets/FiltersView-I1hHnGEc.js
@@ -0,0 +1,1 @@
+import{U as o,P as t,o as r,D as c}from"./index-8zzRjrY0.js";const n={};function s(_,a){const e=t("RouterView");return r(),c(e)}const f=o(n,[["render",s]]);export{f as default};
--- client/assets/FiltersView-i4zC-JUw.js
+++
@@ -1,1 +0,0 @@
-import{U as o,P as t,o as r,D as c}from"./index-wJbtArBJ.js";const n={};function s(_,a){const e=t("RouterView");return r(),c(e)}const f=o(n,[["render",s]]);export{f as default};
