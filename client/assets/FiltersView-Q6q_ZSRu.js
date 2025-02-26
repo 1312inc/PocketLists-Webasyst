4f5fb58deb8d41128a233049ea1699cb017eaf8b
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-02-26 17:47:14 +0300

    Some updates
    right sidebar > auto open on wide ≈1550px+ screens #107.325
    open /map => items won't show up until F5 #107.327

---
+++ client/assets/FiltersView-Q6q_ZSRu.js
@@ -0,0 +1,1 @@
+import{W as o,P as t,o as r,D as c}from"./index-V04OsyDQ.js";const n={};function s(_,a){const e=t("RouterView");return r(),c(e)}const f=o(n,[["render",s]]);export{f as default};
--- client/assets/FiltersView-pXxLTiYB.js
+++
@@ -1,1 +0,0 @@
-import{V as o,P as t,o as r,D as c}from"./index-nZv1CA3w.js";const n={};function s(_,a){const e=t("RouterView");return r(),c(e)}const f=o(n,[["render",s]]);export{f as default};
