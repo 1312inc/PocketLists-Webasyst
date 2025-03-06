20091de0cc80e2cc74e20454561ffeff2474521d
moroz <ivan.moroz@webasyst.ru>
2025-03-06 13:17:42 +0300

    #107.350: PL PRO migrate

--- plugins/gorshochek/lib/pocketlistsGorshochekPlugin.class.php
+++ plugins/gorshochek/lib/pocketlistsGorshochekPlugin.class.php
@@ -4,10 +4,11 @@ class pocketlistsGorshochekPlugin extends waPlugin
 {
     public function pocketlistsGorshochekPluginHookHandlerSettings()
     {
+        $wa_url = wa_url();
         $li = <<<STR
 <li>
     <a href="#/settings/plugin/gorshochek/cook">
-        <i class="icon size-20" style="background-image: url('/wa-apps/pocketlists/plugins/gorshochek/img/gorshochek.png'); background-size: 16px 16px;"></i>
+        <i class="icon size-20" style="background-image: url('{$wa_url}wa-apps/pocketlists/plugins/gorshochek/img/gorshochek.png'); background-size: 16px 16px;"></i>
         <span>Горшочек</span>
     </a>
 </li>
