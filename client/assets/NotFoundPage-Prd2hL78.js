0cce01f358a7609179844a0d3a0d02001e413fac
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-03-12 12:10:56 +0300

    item > comments > fix 'not delivered' issue #107.357

---
+++ client/assets/NotFoundPage-Prd2hL78.js
@@ -0,0 +1,1 @@
+import{H as e}from"./index-U5l9bTvj.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
--- client/assets/NotFoundPage-UNkCHLiV.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./index-mTohFGR9.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
