04bf7ff36c4809c1305b3751159ff9db8add3bfc
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-03-17 17:28:46 +0300

    Major updates
    natural input parser: todos/upnext won't work, unify @ all filters #107.363
    /todos/completed filter #107.358
    natural input > items.add is called whenever input is understood #107.364

---
+++ client/assets/NotFoundPage-izPyKVgt.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-4HvsvwhM.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
--- client/assets/NotFoundPage-szi-YD4P.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-YZh18xm8.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
