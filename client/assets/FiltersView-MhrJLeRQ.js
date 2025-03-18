10b1e4cfc9414d4d9696e8ccbda7b949fe8f22a9
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-03-18 13:26:27 +0300

    Some minor updates

---
+++ client/assets/FiltersView-MhrJLeRQ.js
@@ -0,0 +1,1 @@
+import{_ as o,s as t,o as n,n as r}from"./index-x6sSbXqN.js";const c={};function s(_,a){const e=t("RouterView");return n(),r(e)}const f=o(c,[["render",s]]);export{f as default};
--- client/assets/FiltersView-xvK-tQLG.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,z as t,o as r,s as c}from"./index-ENQg1R7a.js";const n={};function s(_,a){const e=t("RouterView");return r(),c(e)}const f=o(n,[["render",s]]);export{f as default};
