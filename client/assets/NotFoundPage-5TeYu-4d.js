ecec24c08ae999305e9111c064651415bbb9ac60
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-04-08 12:36:17 +0300

    Tune Calendar cell icons #107.417

--- client/assets/NotFoundPage-5TeYu-4d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-0jAVg7TK.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
---
+++ client/assets/NotFoundPage-djYs-stG.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-TI3QLuX7.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
