be08b0c12d5573193cf435992ab5ea77954857f8
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-04-23 21:15:53 +0300

    item textarea-like edit & selection > resolve & marry all UI approaches #107.457

---
+++ client/assets/FiltersView-oO44n_Sl.js
@@ -0,0 +1,1 @@
+import{_ as o,G as t,o as r,x as c}from"./index-qDnc5iof.js";const n={};function s(_,a){const e=t("RouterView");return r(),c(e)}const f=o(n,[["render",s]]);export{f as default};
---
+++ client/assets/NotFoundPage-GDLpMt7B.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-qDnc5iof.js";const r={};function t(c,n){return" 404 "}const _=e(r,[["render",t]]);export{_ as default};
