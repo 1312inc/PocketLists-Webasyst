cfd757d9c6a20583fad41337ce547ce9e520ae57
moroz <ivan.moroz@webasyst.ru>
2025-04-28 16:52:40 +0300

    #107.494: PL API, pocketlists.items.getStream upnext ORDER BY > show all 'favorite == 1'

--- api/v1/pocketlists.items.getStream.method.php
+++ api/v1/pocketlists.items.getStream.method.php
@@ -167,7 +167,7 @@ class pocketlistsItemsGetStreamMethod extends pocketlistsApiAbstractMethod
                 $sql_parts['where']['and'][] = 'i.status = i:status';
                 $sql_parts['where']['and']['def'] = 'i.list_id IN (i:list_ids) OR i.list_id IS NULL';
                 $sql_parts['where']['and'][] = 'i.assigned_contact_id = i:curr_user_id OR uf.contact_id OR (i.contact_id = i:curr_user_id AND i.assigned_contact_id IS NULL AND i.due_date IS NOT NULL)';
-                $sql_parts['order by'][] = 'i.calc_priority DESC, i.due_date ASC, i.due_datetime ASC, i.id';
+                $sql_parts['order by'][] = 'favorite DESC, i.calc_priority DESC, i.due_date ASC, i.due_datetime ASC, i.id';
                 break;
             case 'due':
                 /** due, due/YYYY-MM-DD, due/YYYY-MM-DD,YYYY-MM-DD */
