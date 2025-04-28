a08449cf32556285e465fc4221580041524d3e0e
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-04-28 21:23:48 +0300

    Some fixes #107.482

--- client/assets/SortableList.vue_vue_type_style_index_0_lang-qvv94xRl.js
+++ client/assets/SortableList.vue_vue_type_style_index_0_lang-qvv94xRl.js
@@ -1,4 +1,4 @@
-import{d as Qe,r as tn,m as en,p as nn,aL as ht,o as ie,b as re,F as on,y as rn,n as Oe,R as an}from"./index-ux3jP22C.js";/**!
+import{d as Qe,r as tn,m as en,p as nn,aL as ht,o as ie,b as re,F as on,y as rn,n as Oe,R as an}from"./index-UaupfHCg.js";/**!
  * Sortable 1.15.2
  * @author	RubaXa   <trash@rubaxa.org>
  * @author	owenm    <owen23355@gmail.com>
