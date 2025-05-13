f8660c5f721e4e26be2a68aaeaab74cba7c8686f
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-05-13 14:51:01 +0300

    item list @ stream > display list name & icon #107.564

---
+++ client/assets/useTitle-3p0SJdUt.js
@@ -0,0 +1,1 @@
+import{ac as s,ad as a}from"./index-1o9lfSTD.js";const T=e=>s(e,{titleTemplate:`${e?"%s — ":""}${a}`});export{T as u};
--- client/assets/useTitle-F3SJ4ItY.js
+++
@@ -1,1 +0,0 @@
-import{ac as s,ad as a}from"./index-NV3btlxa.js";const T=e=>s(e,{titleTemplate:`${e?"%s — ":""}${a}`});export{T as u};
