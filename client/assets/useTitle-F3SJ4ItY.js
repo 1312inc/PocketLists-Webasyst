1f2e49fcd2b6b1e07da2db2678b5f84a7882ec4f
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-05-13 12:59:44 +0300

    Todos empty message fix #107.531

---
+++ client/assets/useTitle-F3SJ4ItY.js
@@ -0,0 +1,1 @@
+import{ac as s,ad as a}from"./index-NV3btlxa.js";const T=e=>s(e,{titleTemplate:`${e?"%s — ":""}${a}`});export{T as u};
--- client/assets/useTitle-yIMDeMXv.js
+++
@@ -1,1 +0,0 @@
-import{ac as s,ad as a}from"./index-6bvkz2pH.js";const T=e=>s(e,{titleTemplate:`${e?"%s — ":""}${a}`});export{T as u};
