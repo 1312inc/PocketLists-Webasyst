2ba31b68818eadfb016a0c859a00d87f2baea128
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-05-14 10:17:32 +0300

    client: Major fixes
    map: replace heart symbol with smthng more relevant to map marker #107.535
    item list > arrow won't click #107.540

---
+++ client/assets/useTitle-B-UJ_j2S.js
@@ -0,0 +1,1 @@
+import{ac as s,ad as a}from"./index-Y5jYPgTk.js";const T=e=>s(e,{titleTemplate:`${e?"%s — ":""}${a}`});export{T as u};
--- client/assets/useTitle-l6QBlsgh.js
+++
@@ -1,1 +0,0 @@
-import{ac as s,ad as a}from"./index-h5GcCylm.js";const T=e=>s(e,{titleTemplate:`${e?"%s — ":""}${a}`});export{T as u};
