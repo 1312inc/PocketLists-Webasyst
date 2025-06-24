45c42783afc89ec2b474118268e0418ed2a2b971
Vladimir V. Tuporshin <vofka@vofka.ru>
2025-06-24 15:00:39 +0300

    premium ui promo tweaks

--- lib/class/pocketlistsViewHelper.class.php
+++ lib/class/pocketlistsViewHelper.class.php
@@ -79,13 +79,13 @@ class pocketlistsViewHelper
         {
             $pricing = array( 'compare_price' => '', 'price' => '15 999 <span class="ruble">₽</span>', 'special' => '' );
             if (date('Ymd')<='20250531') $pricing = array( 'compare_price' => '11 999', 'price' => '5 999 <span class="ruble">₽</span>', 'special' => '&minus;50% до 31.05', 'special_short' => '&minus;50% до 31.05', 'special_color' => 'green' );
-            elseif (date('Ymd')<='20250831') $pricing = array( 'compare_price' => '15 999', 'price' => '11 999 <span class="ruble">₽</span>', 'special' => '&minus;25%', 'special_short' => '', 'special_color' => 'orange' );
+            elseif (date('Ymd')<='20250831') $pricing = array( 'compare_price' => '15 999', 'price' => '11 999 <span class="ruble">₽</span>', 'special' => '&minus;25% до 31.08', 'special_short' => '&minus;25%', 'special_color' => 'green' );
         }
         else
         {
             $pricing = array( 'compare_price' => '', 'price' => '$269', 'special' => '' );
             if (date('Ymd')<='20250531') $pricing = array( 'compare_price' => '$199', 'price' => '$99', 'special' => '&minus;50% / 05.31', 'special_short' => '&minus;50%', 'special_color' => 'green' );
-            elseif (date('Ymd')<='20250630') $pricing = array( 'compare_price' => '$269', 'price' => '$199', 'special' => '&minus;26%', 'special_short' => '', 'special_color' => 'orange' );
+            elseif (date('Ymd')<='20250630') $pricing = array( 'compare_price' => '$269', 'price' => '$199', 'special' => '&minus;25% / 08.31', 'special_short' => '&minus;25%', 'special_color' => 'green' );
         }
 
         return $pricing;
