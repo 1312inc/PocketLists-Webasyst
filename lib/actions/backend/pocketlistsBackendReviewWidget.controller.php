863b27f8d62d02d9e55205d9dcbb94c1c6eae711
moroz <ivan.moroz@webasyst.ru>
2025-06-30 19:48:54 +0300

    #107.669: PL: Return reviewWidget render by request

--- lib/actions/backend/pocketlistsBackendReviewWidget.controller.php
+++ lib/actions/backend/pocketlistsBackendReviewWidget.controller.php
@@ -5,6 +5,9 @@ class pocketlistsBackendReviewWidgetController extends waViewAction
     {
         $widget = '';
         $widget_id = waRequest::get('id', '', waRequest::TYPE_STRING_TRIM);
+        $ui = waRequest::get('ui', '2.0', waRequest::TYPE_STRING_TRIM);
+        $ui = (in_array($ui, array('1.3', '2.0')) ? $ui : '2.0');
+        waRequest::setParam('force_ui_version', $ui);
 
         $installer_app = wa()->getView()->getHelper()->installer;
         if (!empty($widget_id) && $installer_app && method_exists($installer_app, 'reviewWidget')) {
