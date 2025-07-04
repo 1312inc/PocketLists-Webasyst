16fc7138e0e6e27b0cdd443ee4b56f5815389caa
moroz <ivan.moroz@webasyst.ru>
2025-07-04 10:56:35 +0300

    #107.653: PL: pl2pro automation > migrate all PHP calls to the main app

--- lib/class/EventListener/pocketlistsShopBackendOrder.class.php
+++ lib/class/EventListener/pocketlistsShopBackendOrder.class.php
@@ -106,7 +106,7 @@ final class pocketlistsShopBackendOrder
      */
     public function onOrderAction(array $params)
     {
-        if (pocketlistsLicensing::isPremium() || pocketlistsHelper::hasPlugin('pro')) {
+        if (pocketlistsLicensing::isPremium() && !pocketlistsHelper::hasPlugin('pro')) {
             try {
                 pocketlistsLogger::debug('in order action handler');
 
