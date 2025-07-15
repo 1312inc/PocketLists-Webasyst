ad2dd530ddbbe8139aeedc31c02ae16bd5dc658f
Konstantin Zaytsev <zaytsev.const@gmail.com>
2025-07-15 13:17:49 +0300

    'My to-dos' to-do list widget @ dashboard #107.672

--- widgets/todos/lib/pocketlistsTodos.widget.php
+++ widgets/todos/lib/pocketlistsTodos.widget.php
@@ -4,6 +4,8 @@ class pocketlistsTodosWidget extends waWidget
 {
     public function defaultAction()
     {
-        $this->display([]);
+        $this->display([
+            'wa_app_static_url' => wa()->getAppStaticUrl(pocketlistsHelper::APP_ID)
+        ]);
     }
 }
