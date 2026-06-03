9d6ba64174d9644e62e536e0cec8a66a8a0046a1
Konstantin Zaytsev <zaytsev.const@gmail.com>
2026-06-03 14:45:23 +0300

    client: build

---
+++ client/assets/FiltersView-_kSyM_cn.js
@@ -0,0 +1,1 @@
+import{Kt as e,Pt as t,Xt as n,l as r}from"./index-CLNRMLnI.js";var i={};function a(r,i){let a=n(`RouterView`);return e(),t(a)}var o=r(i,[[`render`,a]]);export{o as default};
---
+++ client/assets/NotFoundPage-D7HjbhGe.js
@@ -0,0 +1,1 @@
+import{l as e}from"./index-CLNRMLnI.js";var t={};function n(e,t){return` 404 `}var r=e(t,[[`render`,n]]);export{r as default};
