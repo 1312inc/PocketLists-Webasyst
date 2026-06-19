5c77536ac059e6093227b603c409e19f2576e8e9
moroz <ivan.moroz@webasyst.ru>
2026-06-19 19:06:59 +0300

    #107.642: pocketlists service API

--- lib/class/pocketlistsServicesApi.class.php
+++ lib/class/pocketlistsServicesApi.class.php
@@ -16,7 +16,7 @@ class pocketlistsServicesApi extends installerServicesApi
             if (empty($url)) {
                 return false;
             }
-            $net->query($url);
+            $net->query($url, [], waNet::METHOD_POST);
         } catch (waNetTimeoutException $wa_net) {
             return false;
         } catch (waNetException $wa_net) {
